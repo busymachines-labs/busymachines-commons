4f3bd05fcc27c535d838b1fd1df3ce4622e56bd6
Ruud Diterwich <ruud@diterwich.com>
2013-06-21 18:06:25 +0200

    cache changes

--- src/main/scala/com/busymachines/commons/dao/DaoCache.scala
+++ src/main/scala/com/busymachines/commons/dao/DaoCache.scala
@@ -6,31 +6,31 @@ import com.busymachines.commons.domain.Id
 import scala.collection.concurrent.TrieMap
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
 
 class DaoCache [T <: HasId[T]](val dao : Dao[T])(implicit ec : ExecutionContext) {
   
-  private val _idCache = TrieMap[Id[T], Future[Option[Versioned[T]]]]()
-  private val _searchCache = TrieMap[Seq[SearchCriteria[T]], List[T]]()
+  private val _idCache = TrieMap[Id[T], Option[Versioned[T]]]()
+  private val _searchCache = TrieMap[SearchCriteria[T], Future[List[Versioned[T]]]]()
   
-  def retrieve(id: Id[T]): Future[Option[Versioned[T]]] = 
-    _idCache.getOrElseUpdate(id, dao.retrieve(id))
+  def retrieve(id: Id[T], timeout : Duration): Option[Versioned[T]] = 
+    _idCache.getOrElseUpdate(id, Await.result(dao.retrieve(id), timeout))
     
   
-  def retrieve(ids: Seq[Id[T]]): Future[List[Versioned[T]]] = 
-    Future.sequence(ids.toList.map(retrieve(_))).map(_.flatMap(_.toSeq))
-    
-//    val cached = ids.map(id => id -> _idCache.get(id))
-//    val missingIds = cached.collect { case (id, None) => id }
-//    val readItems = missingIds match {
-//      case Seq() => Future.successful(Seq())
-//      case ids => dao.retrieve(ids)
-//    }
-//    val result = cached.collect { case (_, Some(item)) => item } ++ readItems
-//    _itemCache ++= result.map(item => item.id -> item)
-//    result
-//  } 
+  def retrieve(ids: Seq[Id[T]], timeout : Duration): Seq[Versioned[T]] = {
+    val cached = ids.map(id => id -> _idCache.get(id))
+    val missingIds = cached.collect { case (id, None) => id }
+    val retrieved = missingIds match {
+      case Seq() => List()
+      case ids => Await.result(dao.retrieve(ids), timeout)
+    }
+    val result = cached.collect { case (id, Some(v)) => (id -> v) } ++ retrieved.map(v => v.entity.id -> v)
+    _idCache ++= result
+    result
+  } 
     
 
-  def search(criteria : SearchCriteria[T]): Future[List[Versioned[T]]]
+  def search(criteria : SearchCriteria[T]): Future[List[Versioned[T]]] =
+    _searchCache.getOrElseUpdate(criteria, dao.search(criteria))
 
 }
