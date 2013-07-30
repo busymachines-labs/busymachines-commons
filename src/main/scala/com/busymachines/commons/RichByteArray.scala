11445f0640bbe1bc8fa7d2910e82a76e8925b7da
Ruud Diterwich <ruud@diterwich.com>
2013-07-30 11:07:11 +0200

    added crc32

--- src/main/scala/com/busymachines/commons/RichByteArray.scala
+++ src/main/scala/com/busymachines/commons/RichByteArray.scala
@@ -1,12 +1,18 @@
 package com.busymachines.commons
 
 import com.google.common.io.BaseEncoding
+import com.google.common.hash.Hashing
 
 object RichByteArray {
   val base64Encoding = BaseEncoding.base64
+  val crc32 = Hashing.crc32
 }
 
 class RichByteArray(val bytes : Array[Byte]) extends AnyVal {
   def encodeBase64 : String = 
     RichByteArray.base64Encoding.encode(bytes)
+    
+  def crc32 = 
+    RichByteArray.crc32.hashBytes(bytes).asInt()
+    
 }
