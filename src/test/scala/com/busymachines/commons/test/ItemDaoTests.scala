ba4365d32d7715625b3c8e57e2696dadd47e6544
Paul Sabou <paul.sabou@gmail.com>
2013-07-30 11:40:31 +0200

    added test for Date mapping type

--- src/test/scala/com/busymachines/commons/test/ItemDaoTests.scala
+++ src/test/scala/com/busymachines/commons/test/ItemDaoTests.scala
@@ -1,9 +1,7 @@
 package com.busymachines.commons.test
 
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import org.scalatest.FlatSpec
-
 import com.busymachines.commons.dao.Versioned.toEntity
 import com.busymachines.commons.elasticsearch.ESSearchCriteria.Delegate
 import com.busymachines.commons.elasticsearch.EsRootDao
@@ -11,39 +9,47 @@ import com.busymachines.commons.elasticsearch.ESType
 import com.busymachines.commons.implicits.richFuture
 import com.busymachines.commons.test.DomainJsonFormats.itemFormat
 import com.busymachines.commons.testing.EmptyESTestIndex
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
 
 class ItemDaoTests extends FlatSpec with EmptyESTestIndex {
-  
+
   val dao = new EsRootDao[Item](esIndex, ESType("item", ItemMapping))
-  
+
   "ItemDao" should "create & retrieve" in {
-    val item = Item(name = "Sample item", properties = Property(name = "Property1") :: Property(name = "Property2") :: Nil)
+    val now = DateTime.now(DateTimeZone.UTC)
+    val item = Item(name = "Sample item", validUntil = now, properties = Property(name = "Property1") :: Property(name = "Property2") :: Nil)
     dao.create(item, true).await
     assert(dao.retrieve(item.id).await.get.id === item.id)
+    assert(dao.retrieve(item.id).await.get.validUntil === now)
   }
 
   it should "create & update & retrieve" in {
-    val item = Item(name = "Sample item 1", properties = Property(name = "Property1") :: Property(name = "Property2") :: Nil)
+    val now = DateTime.now(DateTimeZone.UTC)
+    val item = Item(name = "Sample item 1", validUntil = now, properties = Property(name = "Property1") :: Property(name = "Property2") :: Nil)
     dao.create(item, true).await
     assert(dao.retrieve(item.id).await.get.id === item.id)
-    dao.modify(item.id)(_=>item.copy(name="Sample item 2")).await
-    assert(dao.retrieve(item.id).await.get.name === "Sample item 2")    
+    dao.modify(item.id)(_ => item.copy(name = "Sample item 2")).await
+    assert(dao.retrieve(item.id).await.get.name === "Sample item 2")
+    assert(dao.retrieve(item.id).await.get.validUntil === now)
   }
-  
+
   it should "create & delete" in {
-    val item = Item(name = "Sample item", properties = Property(name = "Property1") :: Property(name = "Property2") :: Nil)
+    val now = DateTime.now(DateTimeZone.UTC)
+    val item = Item(name = "Sample item", validUntil = now, properties = Property(name = "Property1") :: Property(name = "Property2") :: Nil)
     dao.create(item, true).await
     assert(dao.retrieve(item.id).await.get.id === item.id)
+    assert(dao.retrieve(item.id).await.get.validUntil === now)
     dao.delete(item.id, true).await
     assert(dao.retrieve(item.id).await === None)
   }
 
   it should "create & search for simple nested object" in {
-    val item = Item(name = "Sample item", properties = Property(name = "Property3") :: Property(name = "Property4") :: Nil)
+    val now = DateTime.now(DateTimeZone.UTC)
+    val item = Item(name = "Sample item", validUntil = now, properties = Property(name = "Property3") :: Property(name = "Property4") :: Nil)
     dao.create(item, true).await
     assert(dao.search(ItemMapping.properties / PropertyMapping.name === "Property3").await.size === 1)
+
   }
-  
-  
 
 }
--- src/test/scala/com/busymachines/commons/test/TestDomain.scala
+++ src/test/scala/com/busymachines/commons/test/TestDomain.scala
@@ -5,6 +5,7 @@ import spray.json._
 import com.busymachines.commons.domain.HasId
 import com.busymachines.commons.domain.Id
 import com.busymachines.commons.elasticsearch.ESMapping
+import org.joda.time.DateTime
 
 case class PropertyExternalReference(
   id: Id[PropertyExternalReference] = Id.generate[PropertyExternalReference],
@@ -14,34 +15,36 @@ case class Property(
   id: Id[Property] = Id.generate[Property],
   mandatory: Boolean = false,
   name: String,
-  externalReferences:List[PropertyExternalReference] = Nil) extends HasId[Property]
+  externalReferences: List[PropertyExternalReference] = Nil) extends HasId[Property]
 
 case class Item(
   id: Id[Item] = Id.generate[Item],
+  validUntil: DateTime,
   name: String,
   properties: List[Property] = Nil) extends HasId[Item]
 
 object DomainJsonFormats extends CommonJsonFormats {
   implicit val propertyReferenceFormat = jsonFormat2(PropertyExternalReference)
   implicit val propertyFormat = jsonFormat4(Property)
-  implicit val itemFormat = jsonFormat3(Item)
+  implicit val itemFormat = jsonFormat4(Item)
 }
 
 object PropertyReferenceMapping extends ESMapping[PropertyExternalReference] {
-  val id = "id" -> "_id" as String & NotAnalyzed  
-  val name = "name" as String & NotAnalyzed   
+  val id = "id" -> "_id" as String & NotAnalyzed
+  val name = "name" as String & NotAnalyzed
 }
 
 object PropertyMapping extends ESMapping[Property] {
-  val id = "id" -> "_id" as String & NotAnalyzed  
+  val id = "id" -> "_id" as String & NotAnalyzed
   val mandatory = "mandatory" as Boolean
   val name = "name" as String & NotAnalyzed
   val externalReferences = "externalReferences" -> "external_references" as Nested(PropertyReferenceMapping)
 }
 
 object ItemMapping extends ESMapping[Item] {
-  val id = "id" -> "_id" as String & NotAnalyzed  
+  val id = "id" -> "_id" as String & NotAnalyzed
   val name = "name" as String & NotAnalyzed
+  val validUntil = "validUntil" as Date & NotAnalyzed
   val properties = "properties" -> "item_properties" as Nested(PropertyMapping)
 }
 
