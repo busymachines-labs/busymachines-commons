8db0e651a9389ae3a4289416ecc9d40e00bdbf11
Paul Sabou <paul.sabou@gmail.com>
2013-07-31 18:14:00 +0200

    added indexName to es config

--- src/main/scala/com/busymachines/commons/elasticsearch/ESConfiguration.scala
+++ src/main/scala/com/busymachines/commons/elasticsearch/ESConfiguration.scala
@@ -5,6 +5,7 @@ import com.busymachines.commons.HasConfiguration
 
 class ESConfiguration extends HasConfiguration {
   def clusterName = config.getStringOption("clusterName") getOrElse "elasticsearch"
+  def indexName = config.getString("indexName") 
   def hostNames = config.getStringSeq("hostNames") nonEmptyOrElse Seq("localhost")
   def numberOfShards = config.getIntOption("numberOfShards") getOrElse 1
   def numberOfReplicas = config.getIntOption("numberOfReplicas") getOrElse 0
