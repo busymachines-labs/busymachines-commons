2fd20327679f15cbcfcc904dd5624ca74b824b0c
Paul Sabou <paul.sabou@gmail.com>
2013-08-02 00:19:29 +0200

    increased ES to 0.90.2

--- project/Build.scala
+++ project/Build.scala
@@ -28,7 +28,7 @@ object BusyMachinesCommonsBuild extends Build {
     resolvers += "spray repo" at "http://nightlies.spray.io",
     resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
     libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0.M5b" withSources(),
-    libraryDependencies +=  "org.elasticsearch" % "elasticsearch" % "0.90.1" withSources(),
+    libraryDependencies +=  "org.elasticsearch" % "elasticsearch" % "0.90.2" withSources(),
     libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.2.0-RC2" withSources(),
     libraryDependencies += "com.typesafe.akka" %% "akka-cluster" % "2.2.0-RC2" withSources(),
     libraryDependencies += "com.typesafe.akka" %% "akka-contrib" % "2.2.0-RC2" withSources(),
