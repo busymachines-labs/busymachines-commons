8d7888a7d6cca1f2597a44844b2fe4924e54112f
Ruud Diterwich <ruud@diterwich.com>
2013-08-12 21:08:15 +0200

    deprecated Page.optional

--- src/main/scala/com/busymachines/commons/dao/Page.scala
+++ src/main/scala/com/busymachines/commons/dao/Page.scala
@@ -1,15 +1,17 @@
 package com.busymachines.commons.dao
 
 object Page {
-  val first = Page(0, 10)
-  def optional(from: Option[Int], size: Option[Int]) =
+  val first = new Page(0, 10)
+  @deprecated
+  def optional(from: Option[Int], size: Option[Int]) = apply(from, size)
+  def apply(from: Option[Int], size: Option[Int]) =
     (from, size) match {
       case (None, None) => first
-      case (Some(f), None) => Page(f, 10)
-      case (None, Some(s)) => Page(0, s)
-      case (Some(f), Some(s)) => Page(f, s)
+      case (Some(f), None) => new Page(f, 10)
+      case (None, Some(s)) => new Page(0, s)
+      case (Some(f), Some(s)) => new Page(f, s)
     }
-  def first(size: Int) = Page(0, size)
-  val all = Page(0, 99999999)
+  def first(size: Int) = new Page(0, size)
+  val all = new Page(0, 99999999)
 }
 case class Page(from: Int, size: Int)
