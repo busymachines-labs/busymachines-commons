aedf7f1940dd73e8a2f227513dc2abd4e44ebeba
Paul Sabou <paul.sabou@gmail.com>
2013-08-24 11:21:32 +0200

    took out uneeded credentials models that conflicted

--- src/main/scala/com/busymachines/prefab/authentication/model/Credentials.scala
+++ src/main/scala/com/busymachines/prefab/authentication/model/Credentials.scala
@@ -9,12 +9,3 @@ import com.busymachines.commons.http.AuthenticationToken
 case class Credentials(
   id: Id[Credentials] = Id.generate,
   passwordCredentials: Option[PasswordCredentials] = None) extends HasId[Credentials]
-
-case class PasswordCredentials(
-  salt: String = Random.nextString(8),
-  passwordHash: Array[Byte] = Array.empty) {
-
-  def withPassword(password: String) =
-    this.copy(passwordHash = (salt + password).sha256Hash)
-}
-
