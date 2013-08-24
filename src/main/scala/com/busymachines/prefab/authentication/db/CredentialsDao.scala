da70f912895a71f572a74c1a88d0bb135d1c1491
Paul Sabou <paul.sabou@gmail.com>
2013-08-24 18:58:23 +0200

    credentials dao extends rootdao so we can create credentials

--- src/main/scala/com/busymachines/prefab/authentication/db/CredentialsDao.scala
+++ src/main/scala/com/busymachines/prefab/authentication/db/CredentialsDao.scala
@@ -2,5 +2,6 @@ package com.busymachines.prefab.authentication.db
 
 import com.busymachines.commons.dao.Dao
 import com.busymachines.prefab.authentication.model.Credentials
+import com.busymachines.commons.dao.RootDao
 
-trait CredentialsDao extends Dao[Credentials]
+trait CredentialsDao extends RootDao[Credentials]
