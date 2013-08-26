abaeedd528966af463b3d88435a099525e2eae5d
Adrian Bumbas <a.bumbas@gmail.com>
2013-08-26 16:10:10 +0300

    Added cache directive to the commons

--- src/main/scala/com/busymachines/commons/http/CommonHttpService.scala
+++ src/main/scala/com/busymachines/commons/http/CommonHttpService.scala
@@ -5,13 +5,18 @@ import spray.routing.HttpService
 import spray.routing.RequestContext
 import spray.routing.Route
 import akka.actor.ActorRefFactory
+import scala.concurrent.duration.FiniteDuration
+import spray.http.HttpHeaders
+import spray.http.CacheDirectives
 
-abstract class CommonHttpService(implicit val actorRefFactory : ActorRefFactory) extends HttpService with CommonDirectives with Route with Logging  {
-
+abstract class CommonHttpService(implicit val actorRefFactory: ActorRefFactory) extends HttpService with CommonDirectives with Route with Logging {
   implicit def executionContext = actorRefFactory.dispatcher
-  
+
   def apply(request: RequestContext) =
     route(request)
 
-  def route : Route
+  def route: Route
+
+  def cacheHeaders(duration: FiniteDuration) =
+    respondWithHeaders(HttpHeaders.`Cache-Control`(CacheDirectives.`private`()), HttpHeaders.`Access-Control-Max-Age`(duration.toSeconds))
 }
