e446042895d56289a31d960900e86188a7f3cba0
Paul Sabou <paul.sabou@gmail.com>
2013-08-31 20:25:54 +0200

    date time converter

--- src/main/scala/com/busymachines/commons/elasticsearch/JsValueConverter.scala
+++ src/main/scala/com/busymachines/commons/elasticsearch/JsValueConverter.scala
@@ -3,6 +3,7 @@ package com.busymachines.commons.elasticsearch
 import spray.json.JsValue
 import spray.json.JsString
 import spray.json.JsNumber
+import org.joda.time.DateTime
 
 object JsValueConverter {
   implicit val stringConverter = new JsValueConverter[String] {
@@ -26,6 +27,14 @@ object JsValueConverter {
       value match {
         case JsString(s) => s toBoolean
         case other => false
+      }
+  }
+
+  implicit val dateConverter = new JsValueConverter[DateTime] {
+    def convert(value: JsValue): DateTime =
+      value match {
+        case JsString(s) => DateTime.parse(s)
+        case other => throw new IllegalArgumentException(s"Cannot convert string $other to date time")
       }
   }
 
