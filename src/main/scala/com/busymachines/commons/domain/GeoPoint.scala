3f3d1431e4630cc6baabf9775937520f1f553c0f
Ruud Diterwich <ruud@diterwich.com>
2013-10-21 23:11:35 +0200

    added geopoint parser

--- src/main/scala/com/busymachines/commons/domain/GeoPoint.scala
+++ src/main/scala/com/busymachines/commons/domain/GeoPoint.scala
@@ -2,4 +2,16 @@ package com.busymachines.commons.domain
 
 case class GeoPoint(
   lat: Double,
-  lon: Double)
+  lon: Double) {
+  override def toString = 
+    lat.toString + "," + lon.toString
+}
+  
+object GeoPointParser {
+  def parse(s : String) : GeoPoint = {
+    s.indexOf(',') match {
+      case -1 => throw new NumberFormatException(s"Not a valid lat/lon pair: $s")
+      case i => GeoPoint(s.substring(0, i).toDouble, s.substring(i + 1).toDouble)
+    }
+  }
+}
