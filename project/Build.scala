f430c1da0ae6796af9c2c5a957ee302978b11735
Ruud Diterwich <ruud@diterwich.com>
2013-11-07 12:26:40 +0100

    added pegdown

--- project/Build.scala
+++ project/Build.scala
@@ -30,6 +30,7 @@ object BusyMachinesCommonsBuild extends Build {
     resolvers += "spray repo" at "http://repo.spray.io",
     resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
     libraryDependencies += "org.scalatest" %% "scalatest" % "2.0" withSources(),
+    libraryDependencies += "org.pegdown" % "pegdown" % "1.4.1" withSources(), // used by scalatest
     libraryDependencies +=  "org.elasticsearch" % "elasticsearch" % "0.90.5" withSources(),
     libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.2.3" withSources(),
     libraryDependencies += "com.typesafe.akka" %% "akka-cluster" % "2.2.3" withSources(),
