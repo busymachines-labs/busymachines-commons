4cecbd4d7ccfce369d95663926e44c8613678d11
Ruud Diterwich <ruud@diterwich.com>
2013-11-18 16:41:25 +0100

    refactored authentication api

--- src/main/scala/com/busymachines/prefab/party/api/v1/AuthenticationApi.scala
+++ src/main/scala/com/busymachines/prefab/party/api/v1/AuthenticationApi.scala
@@ -14,6 +14,7 @@ import com.busymachines.prefab.party.logic.UserAuthenticator
 import com.busymachines.prefab.party.domain.User
 import com.busymachines.prefab.party.api.v1.model.AuthenticationRequest
 import com.busymachines.prefab.party.service.SecurityContext
+import com.busymachines.prefab.authentication.model.Authentication
 
 /**
  * Handling authentication before using API.
@@ -21,80 +22,68 @@ import com.busymachines.prefab.party.service.SecurityContext
 class AuthenticationApiV1(authenticator: UserAuthenticator)(implicit actorRefFactory: ActorRefFactory) extends CommonHttpService with PartyApiV1Directives {
   
   def route: RequestContext => Unit =
-//    path("users" / "authentication") { 
-//      post {
-//       entity(as[Credentials]) { 
-//         case Credentials(email, password, _) =>
-//           authenticator.authenticateUser(email, password).map {
-//             case Some()
-//           }
-//       }
-//      }
-//    } ~
-    path("users" / MatchId[User] / "authentication") { userId =>
-      // Check if user is authenticated.
-      get {
-        headerValueByName(tokenKey) { tokenValue =>
-          Await.result(authenticator.authenticate(Id(tokenValue)), 1.minute) match {
-            case Some(session) => {
-              complete {
-                Map("message" -> s"Partner with token $tokenValue is logged in")
+    // Log in a specific user. Password will be in the body, in json format.  
+    path("users" / "authentication") { 
+      post {
+        entity(as[AuthenticationRequest]) { request =>
+          Await.result(authenticator.authenticateWithLoginNamePassword(request.loginName, request.password), 1.minute) match {
+            case Some(SecurityContext(tenantId, partyId, userId, loginName, authenticationId, permissions)) => {
+              val message = "User %s has been succesfully logged in".format(request.loginName)
+              debug(message)
+              respondWithHeader(RawHeader(tokenKey, authenticationId.toString)) {
+                complete {
+                  // Return authenticated user id.
+                      Map("authToken" -> authenticationId.toString, "userId" -> userId.toString, "partyId" -> partyId.toString)
+                }
               }
             }
             case None => {
+              debug("Tried to log in user %s but received 'Invalid userName or password.'".format(request.loginName))
               respondWithStatus(StatusCodes.NotFound) {
                 complete {
-                  Map("message" -> s"Partner with token $tokenValue is not logged in")
+                  Map("message" -> "Invalid userName or password.")
                 }
               }
             }
           }
         }
-      } ~
-        // Log in a specific user. Password will be in the body, in json format.  
-        post {
-          entity(as[AuthenticationRequest]) { request =>
-            Await.result(authenticator.authenticateWithLoginNamePassword(request.loginName, request.password), 1.minute) match {
-              case Some(SecurityContext(tenantId, partyId, userId, loginName, authenticationId, permissions)) => {
-                val message = "User %s has been succesfully logged in".format(request.loginName)
-                debug(message)
-                respondWithHeader(RawHeader(tokenKey, authenticationId.toString)) {
-                  complete {
-                    // Return authenticated user id.
-                        Map("userId" -> userId)
-                  }
-                }
-              }
-              case None => {
-                debug("Tried to log in user %s but received 'Invalid userName or password.'".format(request.loginName))
-                respondWithStatus(StatusCodes.NotFound) {
-                  complete {
-                    Map("message" -> "Invalid userName or password.")
-                  }
-                }
+      } 
+    } ~    
+    // Check if an authentication token is still valid
+    path("users" / "authentication" / MatchId[Authentication]) { tokenValue =>
+      get {
+        Await.result(authenticator.authenticate(tokenValue), 1.minute) match {
+          case Some(session) => {
+            complete {
+              Map("message" -> s"Authentication token is valid")
+            }
+          }
+          case None => {
+            respondWithStatus(StatusCodes.NotFound) {
+              complete {
+                Map("message" -> s"Authentication token is not valid")
               }
             }
           }
-        } ~
-        // Log out a specific user.
-        delete {
-          headerValueByName(tokenKey) { tokenValue =>
-            Await.result(authenticator.authenticate(Id(tokenValue)), 1.minute) match {
-              case Some(securityContext) => {
-                authenticator.deauthenticate(Id(tokenValue))
-                val message = s"User ${securityContext.user} has been succesfully logged out"
-                debug(message)
+        }
+      }~
+      // Log out a specific user.
+      delete {
+          Await.result(authenticator.authenticate(tokenValue), 1.minute) match {
+            case Some(securityContext) => {
+              authenticator.deauthenticate(tokenValue)
+              val message = s"User ${securityContext.user} has been succesfully logged out"
+              debug(message)
+              complete {
+                Map("message" -> message)
+              }
+            }
+            case None => {
+              val message = "User already logged out."
+              debug(message)
+              respondWithStatus(StatusCodes.NotFound) {
                 complete {
                   Map("message" -> message)
-                }
-              }
-              case None => {
-                val message = "User already logged out."
-                debug(message)
-                respondWithStatus(StatusCodes.NotFound) {
-                  complete {
-                    Map("message" -> message)
-                  }
                 }
               }
             }
