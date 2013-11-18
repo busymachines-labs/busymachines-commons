d6659efc9c55dda89bc26b1f874156c9df8f7089
Ruud Diterwich <ruud@diterwich.com>
2013-11-18 16:14:34 +0100

    relaxed MatchId

--- src/main/scala/com/busymachines/commons/spray/CommonDirectives.scala
+++ src/main/scala/com/busymachines/commons/spray/CommonDirectives.scala
@@ -4,13 +4,14 @@ import spray.routing.PathMatcher
 import spray.routing.PathMatcher.PathMatcher1Ops
 import spray.routing.PathMatcher.regex2PathMatcher
 import com.busymachines.commons.domain.Id
+import spray.routing.PathMatchers.Segment
 
 object CommonDirectives extends CommonDirectives {
-  val idPathMatcher = PathMatcher("""[\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12}""".r)
+  val uuidPathMatcher = PathMatcher("""[\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12}""".r)
 }
 
 trait CommonDirectives {
-  def MatchId[A] = CommonDirectives.idPathMatcher.flatMap { string =>
+  def MatchId[A] = Segment.flatMap { string =>
     try Some(Id[A](string))
     catch {
       case _: IllegalArgumentException => None
