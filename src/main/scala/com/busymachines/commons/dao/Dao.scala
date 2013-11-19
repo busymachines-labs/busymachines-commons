17e4fd2cd8881d584feb9843216720bb40f670e7
Ruud Diterwich <ruud@diterwich.com>
2013-11-19 22:24:58 +0100

    refactored Dao.searchSingle

--- src/main/scala/com/busymachines/commons/dao/Dao.scala
+++ src/main/scala/com/busymachines/commons/dao/Dao.scala
@@ -19,7 +19,7 @@ trait Dao[T <: HasId[T]] {
 
   def search(criteria : SearchCriteria[T], page : Page = Page.first, sort:SearchSort = ESSearchSort.asc("_id"), facets: Seq[FacetField] = Seq.empty): Future[SearchResult[T]]
 
-  def searchSingle(criteria : SearchCriteria[T], f : SearchCriteria[T] => Unit): Future[Option[Versioned[T]]]
+  def searchSingle(criteria : SearchCriteria[T], onMany : List[Versioned[T]] => Versioned[T] = throw new MoreThanOneResultException): Future[Option[Versioned[T]]]
 
   def modify(id: Id[T], reindex : Boolean = true)(f : T => T): Future[Versioned[T]]
   
--- src/main/scala/com/busymachines/commons/dao/Exceptions.scala
+++ src/main/scala/com/busymachines/commons/dao/Exceptions.scala
@@ -3,3 +3,5 @@ package com.busymachines.commons.dao
 class IdAlreadyExistsException(val id : String, val `type` : String) extends Exception(s"${`type`.capitalize} with id $id already exists")
 
 class IdNotFoundException(val id : String, val `type` : String) extends Exception(s"${`type`.capitalize} with id $id not found")
+
+class MoreThanOneResultException(msg : String = "") extends Exception(msg)
