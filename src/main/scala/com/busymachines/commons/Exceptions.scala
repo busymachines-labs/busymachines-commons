e2aac0d615eb5c56d28bf56c50c3c1c577cb19fe
Ruud Diterwich <ruud@diterwich.com>
2013-11-20 22:20:56 +0100

    Added a default exception handler to HttpServer

--- src/main/scala/com/busymachines/commons/Exceptions.scala
+++ src/main/scala/com/busymachines/commons/Exceptions.scala
@@ -1,5 +1,14 @@
 package com.busymachines.commons
 
+import com.busymachines.commons.domain.Id
+
 class NotAuthenticatedException(msg : String, cause : Option[Throwable] = None) extends Exception(msg, cause.getOrElse(null))
 class NotAuthorizedException(msg:String, cause: Option[Throwable] = None) extends Exception(msg, cause.getOrElse(null))
-class EntityNotFoundException(msg:String, cause: Option[Throwable] = None) extends Exception(msg, cause.getOrElse(null))
+/**
+ * Thrown when a specified id was not valid. The message should be descriptive, contain the , english and is meant for end-user
+ * consumption.
+ */
+class EntityNotFoundException(val id : String, val `type` : String) extends Exception(s"${`type`.capitalize} with id $id not found") {
+  def this(id : Id[_], `type` : String) = this(id.toString, `type`)
+}
+
--- src/main/scala/com/busymachines/commons/spray/HttpServer.scala
+++ src/main/scala/com/busymachines/commons/spray/HttpServer.scala
@@ -1,13 +1,13 @@
 package com.busymachines.commons.spray
 
 import com.busymachines.commons.Logging
-
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.event.Logging.DebugLevel
 import akka.io.IO
 import spray.can.Http
 import spray.http.HttpRequest
+import spray.http.StatusCodes
 import spray.routing.Directive.pimpApply
 import spray.routing.HttpService.pimpRouteWithConcatenation
 import spray.routing.HttpServiceActor
@@ -17,12 +17,17 @@ import spray.routing.directives.LogEntry
 import spray.routing.ExceptionHandler
 import spray.routing.HttpService
 import spray.routing.RejectionHandler
+import spray.http.StatusCodes
+import com.busymachines.commons.EntityNotFoundException
+import com.busymachines.commons.NotAuthorizedException
+import com.busymachines.commons.EntityNotFoundException
+import com.busymachines.commons.domain.CommonJsonFormats._
 
-abstract class HttpServer(implicit actorSystem : ActorSystem) extends CommonHttpService with Logging {
+abstract class HttpServer(implicit actorSystem : ActorSystem) extends CommonHttpService with CORSDirectives with Logging {
 
   val interface : String = "localhost"
   val port : Int = 8080
-  val exceptionHandler : ExceptionHandler = ExceptionHandler.default
+  val exceptionHandler : ExceptionHandler = commonExceptionHandler
   def rejectionHandler : RejectionHandler = RejectionHandler.Default
   val routingSettings = RoutingSettings(actorSystem)
 
@@ -36,9 +41,24 @@ abstract class HttpServer(implicit actorSystem : ActorSystem) extends CommonHttp
     val theRoute = route
     def receive = runRoute(logRequest(showRequest _) { theRoute })
     def showRequest(request: HttpRequest) = LogEntry("URL: " + request.uri + "\n CONTENT: " + request.entity, DebugLevel)
-
   }
 
   def start =
     IO(Http) ! Http.Bind(actorSystem.actorOf(Props(new Actor), "http-server"), interface = interface, port = port)
+
+    
+  def commonExceptionHandler = ExceptionHandler.default orElse ExceptionHandler {
+     case e: NotAuthorizedException => crossDomain {
+      debug(s"Exception handler rejecting with forbidden because exception : $e")
+      complete(StatusCodes.Unauthorized)
+    }
+    case e: EntityNotFoundException => crossDomain { ctx =>
+      ctx.complete(StatusCodes.NotFound, Map("message" -> e.getMessage, "id" -> e.id, "type" -> e.`type`).toString)
+    }
+    case e: Throwable => crossDomain { ctx =>
+      error(s"Request ${ctx.request} could not be handled normally: ${e.getMessage}", e)
+      ctx.complete(StatusCodes.InternalServerError, e.getMessage)
+    }
+ 
+  }
 }
