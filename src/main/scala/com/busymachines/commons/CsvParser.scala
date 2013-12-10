b077332dd37de5ae0cfe8cd310d682255c322c5a
Ruud Diterwich <ruud@diterwich.com>
2013-12-10 14:17:36 +0100

    Much faster CSV parser

--- src/main/scala/com/busymachines/commons/CsvParser.scala
+++ src/main/scala/com/busymachines/commons/CsvParser.scala
@@ -2,40 +2,55 @@ package com.busymachines.commons
 
 import scala.util.parsing.combinator._
 import scala.language.postfixOps
+import scala.collection.mutable.ArrayBuilder
+import scala.collection.mutable.ArrayBuffer
+import java.io.Reader
+import java.io.BufferedReader
 
 /**
- * CSV parser based  on RFC4180
+ * CSV parser compatible with RFC4180
  * @see http://tools.ietf.org/html/rfc4180
  */
-object CSVParser {
-  object Parser extends RegexParsers {
-    override val skipWhitespace = false   // meaningful spaces in CSV
+object CsvParser {
   
-    def COMMA   = ","
-    def DQUOTE  = "\""
-    def DQUOTE2 = "\"\"" ^^ { case _ => "\"" }  // combine 2 dquotes into 1
-    def CRLF    = "\r\n" | "\n"
-    def TXT     = "[^\",\r\n]".r
-    def SPACES  = "[ \t]+".r
-  
-    def file: Parser[List[List[String]]] = repsep(record, CRLF) <~ (CRLF?)
-  
-    def record: Parser[List[String]] = repsep(field, COMMA)
-  
-    def field: Parser[String] = escaped|nonescaped
-  
-  
-    def escaped: Parser[String] = {
-      ((SPACES?)~>DQUOTE~>((TXT|COMMA|CRLF|DQUOTE2)*)<~DQUOTE<~(SPACES?)) ^^ { 
-        case ls => ls.mkString("")
+  def parse(reader : Reader) : Array[Array[String]] = {
+    val bufferedReader = new BufferedReader(reader)
+    val result = ArrayBuffer[Array[String]]()
+    var currentLine = ArrayBuffer[String]()
+    var currentField = new StringBuilder
+    var peek = bufferedReader.read
+    var current = bufferedReader.read
+    def goNext { current = peek; peek = bufferedReader.read; if (current == '\r') goNext }
+    def skipWhitespace = while (current == ' ' || current == '\t') goNext
+    while (current != -1) {
+      currentLine.clear
+      skipWhitespace
+      while (current != '\n') {
+        currentField.clear
+        if (current == '"') {
+          goNext
+          while (current != -1 && (current != '"' || peek == '"')) {
+            if (current == '"') goNext
+            currentField append current.asInstanceOf[Char]
+            goNext
+          }
+          if (current == '"') goNext
+          skipWhitespace
+        }
+        else {
+          while (current != -1 && current != ',' && current != '\n') {
+            currentField append current.asInstanceOf[Char]
+            goNext
+          }
+        }
+        if (current == ',') goNext
+        skipWhitespace
+        currentLine += currentField.toString.trim
       }
+      if (current == '\n') goNext
+      if (!currentLine.isEmpty) 
+        result += currentLine.toArray
     }
-  
-    def nonescaped: Parser[String] = (TXT*) ^^ { case ls => ls.mkString("") }
-  }
-  
-  def parse(s: String) = Parser.parseAll(Parser.file, s) match {
-    case Parser.Success(res, _) => res
-    case e => throw new Exception(e.toString)
+    result.toArray
   }
 }
