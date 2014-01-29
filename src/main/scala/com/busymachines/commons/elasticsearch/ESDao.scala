40c6ad46d00ad2ed9b647472c8b1bce594dc198b
Paul Sabou <paul.sabou@gmail.com>
2014-01-29 12:18:21 +0100

    moved the query all methods in the ESDao as they are generic to all DAO's

--- src/main/scala/com/busymachines/commons/elasticsearch/ESDao.scala
+++ src/main/scala/com/busymachines/commons/elasticsearch/ESDao.scala
@@ -13,36 +13,54 @@ import com.busymachines.commons.Logging
 import com.busymachines.commons.dao.SearchResult
 import com.busymachines.commons.dao.MoreThanOneResultException
 import com.busymachines.commons.dao.SearchSort
+import com.busymachines.commons.domain.CommonJsonFormats._
 
 abstract class ESDao[T <: HasId[T]: JsonFormat](val typeName: String)(implicit ec: ExecutionContext) extends Dao[T] with Logging {
 
-  def escapeQueryText(queryText:String) =     
+  /**
+   * Escapes a string special ES characters as specified here : {@link http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#_reserved_characters}
+   */
+  def escapeQueryText(queryText: String) =
     new StringBuilder(queryText).
-    replaceAllLiterally("\\", "\\\\").
-    replaceAllLiterally("/", "\\/").
-    replaceAllLiterally(" ", "\\ ").
-    replaceAllLiterally("+", "\\+").
-    replaceAllLiterally("-", "\\-").
-    replaceAllLiterally("&&", "\\&&").
-    replaceAllLiterally("||", "\\||").
-    replaceAllLiterally("!", "\\!").
-    replaceAllLiterally("(", "\\(").
-    replaceAllLiterally(")", "\\)").
-    replaceAllLiterally("{", "\\{").
-    replaceAllLiterally("}", "\\}").
-    replaceAllLiterally("[", "\\[").
-    replaceAllLiterally("]", "\\]").
-    replaceAllLiterally("^", "\\^").
-    replaceAllLiterally("\"", "\\\"").
-    replaceAllLiterally("~", "\\~").
-    replaceAllLiterally("*", "\\*").
-    replaceAllLiterally("?", "\\?").
-    replaceAllLiterally(":", "\\:").toString
-  
-  def all:SearchCriteria[T] = ESSearchCriteria.All[T]
-  
-  def defaultSort:SearchSort = ESSearchSort.asc("_id")
-  
+      replaceAllLiterally("\\", "\\\\").
+      replaceAllLiterally("/", "\\/").
+      replaceAllLiterally(" ", "\\ ").
+      replaceAllLiterally("+", "\\+").
+      replaceAllLiterally("-", "\\-").
+      replaceAllLiterally("&&", "\\&&").
+      replaceAllLiterally("||", "\\||").
+      replaceAllLiterally("!", "\\!").
+      replaceAllLiterally("(", "\\(").
+      replaceAllLiterally(")", "\\)").
+      replaceAllLiterally("{", "\\{").
+      replaceAllLiterally("}", "\\}").
+      replaceAllLiterally("[", "\\[").
+      replaceAllLiterally("]", "\\]").
+      replaceAllLiterally("^", "\\^").
+      replaceAllLiterally("\"", "\\\"").
+      replaceAllLiterally("~", "\\~").
+      replaceAllLiterally("*", "\\*").
+      replaceAllLiterally("?", "\\?").
+      replaceAllLiterally(":", "\\:").toString
+
+  def allSearchTextOrSearchQuery(searchText: Option[String], searchQuery: Option[String]):Option[SearchCriteria[T]] =
+    (searchText, searchQuery) match {
+      case (Some(qT), None) => Some(allSearchText(qT))
+      case (None, Some(q)) => Some(allSearchQuery(q))
+      case (Some(qT), Some(q)) => Some(allSearchQuery(q))
+      case _ => None
+    }
+
+  def allSearchText(searchText: String): SearchCriteria[T] =
+    allSearchQuery(s"*${escapeQueryText(searchText)}*")
+
+  def allSearchQuery(searchQuery: String): SearchCriteria[T] =
+    (ESMapping._all queryString searchQuery).asInstanceOf[ESSearchCriteria[T]]
+
+  def all: SearchCriteria[T] = ESSearchCriteria.All[T]
+
+  def defaultSort: SearchSort = ESSearchSort.asc("_id")
+
   def searchSingle(criteria: SearchCriteria[T], onMany: List[Versioned[T]] => Versioned[T]): Future[Option[Versioned[T]]] = {
     search(criteria).map(_ match {
       case SearchResult(Nil, _, _) => None
