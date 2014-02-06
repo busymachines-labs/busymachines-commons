5ddc557b2641f7c2cfde79da77c19a4b78d053b9
Paul Sabou <paul.sabou@gmail.com>
2014-02-06 13:06:48 +0100

    fixed example with auth token

--- src/test/scala/com/busymachines/commons/test/AssemblyTestBase.scala
+++ src/test/scala/com/busymachines/commons/test/AssemblyTestBase.scala
@@ -2,12 +2,13 @@ package com.busymachines.commons.test
 
 import akka.actor.ActorSystem
 import com.busymachines.commons.event.LocalEventBus
-import com.busymachines.commons.testing.EmptyESTestIndex
-import com.busymachines.prefab.party.logic.PartyFixture
-import com.busymachines.prefab.party.PartyAssembly
 import com.busymachines.commons.implicits._
-import spray.testkit.{ScalatestRouteTest, RouteTest}
+import com.busymachines.commons.testing.EmptyESTestIndex
+import com.busymachines.prefab.party.PartyAssembly
+import com.busymachines.prefab.party.logic.PartyFixture
 import org.scalatest.FlatSpec
+import spray.testkit.{RouteTest, ScalatestRouteTest}
+
 
 trait AssemblyTestBase extends FlatSpec with PartyAssembly with RouteTest with ScalatestRouteTest {
 
--- src/test/scala/com/busymachines/commons/test/prefab/party/api/UsersApiTests.scala
+++ src/test/scala/com/busymachines/commons/test/prefab/party/api/UsersApiTests.scala
@@ -1,9 +1,9 @@
 package com.busymachines.commons.test.prefab.party.api
 
-import com.busymachines.commons.Logging
 import com.busymachines.commons.test.AssemblyTestBase
 import com.busymachines.prefab.party.api.v1.PartyApiV1Directives
 import com.busymachines.prefab.party.api.v1.model.AuthenticationResponse
+import com.busymachines.prefab.party.logic.PartyFixture
 import org.scalatest.FlatSpec
 import spray.http._
 import spray.json.JsonParser
@@ -11,7 +11,7 @@ import spray.json.JsonParser
 /**
  * Created by alex on 2/6/14.
  */
-class UsersApiTests extends FlatSpec with AssemblyTestBase with PartyApiV1Directives {
+class UsersApiTests extends FlatSpec with AssemblyTestBase with PartyApiV1Directives with PartyFixture {
 
   val userAuthRequestBodyJson = """
     {
@@ -22,7 +22,6 @@ class UsersApiTests extends FlatSpec with AssemblyTestBase with PartyApiV1Direct
 
   val userRequestBodyJson = """
     {
-      "auth-Token":"eec70494-3390-48da-89cd-89e58d851be3"
       "id": "usr1",
       "credentials": "cred1",
       "firstname": "User",
@@ -34,27 +33,19 @@ class UsersApiTests extends FlatSpec with AssemblyTestBase with PartyApiV1Direct
       "roles":""
     }"""
 
-  "UsersApi" should "get a list of users" in{
+  "UsersApi" should "get one user" in {
 
     var authResponse:AuthenticationResponse=null;
     Post("/users/authentication", HttpEntity(ContentTypes.`application/json`,userAuthRequestBodyJson)) ~> authenticationApiV1.route ~>  check {
       assert(status === StatusCodes.OK)
       assert(body.toString.contains("authToken"))
-      println(body.toString())
       authResponse = JsonParser(body.asString).convertTo[AuthenticationResponse]
     }
 
-    Get(s"/users/${authResponse.authToken}") ~>usersApiV1.route ~>  check {
-      //assert(status === StatusCodes.OK)
-
+    Get(s"/users/$testUser1Id") ~> addHeader("Auth-Token", authResponse.authToken) ~> usersApiV1.route ~> check {
+      assert(status === StatusCodes.OK)
+      assert(body.toString.contains("test-user-1"))
       println(body.asString)
     }
-
-    /*
-    Post("/users/usr1", HttpEntity(ContentTypes.`application/json`,userRequestBodyJson)) ~> usersApiV1.route ~>  check {
-      //assert(status === StatusCodes.OK)
-      //assert(body.toString.contains("authToken"))
-    }
-    */
   }
 }
