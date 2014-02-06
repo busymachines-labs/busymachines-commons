aa58344d1fbff2aae976903a797e4687bf7075ac
Paul Sabou <paul.sabou@gmail.com>
2014-02-06 15:41:24 +0100

    fixed mail model & mail test settings

--- src/main/scala/com/busymachines/commons/mail/OutgoingMailBox.scala
+++ src/main/scala/com/busymachines/commons/mail/OutgoingMailBox.scala
@@ -1,48 +1,46 @@
 package com.busymachines.commons.mail
 
-import com.busymachines.commons.Logging
 import java.util.Properties
-import javax.mail.{Authenticator, PasswordAuthentication, Session}
-import com.busymachines.commons.mail.model.MailMessage
-import javax.mail.internet.MimeBodyPart
-import javax.mail.Message.RecipientType
-import org.joda.time.DateTime
-import javax.activation.DataHandler
 
-class SpecificAuthenticator(userName: String, password: String) extends Authenticator {
-  override def getPasswordAuthentication: PasswordAuthentication = {
-    new PasswordAuthentication(userName, password)
-  }
-}
+import com.busymachines.commons.Logging
+import com.busymachines.commons.mail.model.MailMessage
+import javax.mail.Session
 
 /**
  * Created by paul on 2/6/14.
  */
 class OutgoingMailBox(mailConfig: OutgoingMailConfig) extends Logging {
 
-  val session = Session.getDefaultInstance(serverProperties,
-    new SpecificAuthenticator(mailConfig.userName, mailConfig.password))
+  val session = Session.getInstance(serverProperties)
 
   private def serverProperties: Properties = {
 
     val properties = new Properties
 
     if (mailConfig.ssl) {
-      properties.put("mail.smtp.socketFactory.port", mailConfig.port.toString)
+      properties.put("mail.smtp.socketFactory.port", String.valueOf(mailConfig.port))
       properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
-    } else {
-      properties.put("mail.smtp.starttls.enable", mailConfig.starttls.toString)
     }
-    properties.put("mail.smtp.auth", mailConfig.auth.toString)
+    if (mailConfig.starttls) {
+      properties.put("mail.smtp.starttls.enable", String.valueOf(mailConfig.starttls))
+    }
+    properties.put("mail.smtp.auth", String.valueOf(mailConfig.auth))
     properties.put("mail.smtp.host", mailConfig.host)
-    properties.put("mail.smtp.port", mailConfig.port.toString)
+    properties.put("mail.smtp.port", String.valueOf(mailConfig.port))
 
+    println(properties)
     properties
   }
 
   def send(message: MailMessage) = {
     val messageToSend = MailBox.mailMessageToMessage(session,message)
-    session.getTransport.sendMessage(messageToSend,messageToSend.getAllRecipients)
+    val transport = session.getTransport("smtp")
+    try {
+      transport.connect(mailConfig.userName, mailConfig.password)
+      transport.sendMessage(messageToSend, messageToSend.getAllRecipients)
+    } finally {
+      transport.close()
+    }
   }
 
 
