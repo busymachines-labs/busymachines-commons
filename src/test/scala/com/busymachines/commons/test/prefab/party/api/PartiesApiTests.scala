17865f3d9e0746d34b5d7e543ae87bbb32bdf33f
mateialexandru <alexandru.matei@busymachines.com>
2014-02-07 09:18:31 +0200

    added create one party -needs a fix#12

--- src/test/scala/com/busymachines/commons/test/prefab/party/api/PartiesApiTests.scala
+++ src/test/scala/com/busymachines/commons/test/prefab/party/api/PartiesApiTests.scala
@@ -8,6 +8,10 @@ import com.busymachines.prefab.party.api.v1.model.AuthenticationResponse
 import spray.http.{StatusCodes, ContentTypes, HttpEntity}
 import spray.json.JsonParser
 import com.busymachines.prefab.party.domain.Party
+import com.busymachines.prefab.party.db
+import com.busymachines.commons.elasticsearch
+import com.busymachines.commons.elasticsearch.RichJsValue
+import com.busymachines.prefab.party.db.PartyMapping
 
 /**
  * Created by alex on 2/7/14.
@@ -20,6 +24,38 @@ class PartiesApiTests extends FlatSpec with AssemblyTestBase with PartyApiV1Dire
       "password": "test"
     }
                                 """
+  val partyRequestBodyJson = """
+      {
+      "id": "test-party-2",
+      "tenant": "test-tenant-2",
+      "company": {
+          "name": "Test Company 2"
+        },
+      "addresses": [{
+          "street": "Korenmolen 2",
+          "houseNumber": "4",
+          "postalCode": "1541RW",
+          "city": "Koog aan de Zaan"
+        }],
+      "phoneNumbers": [],
+      "emailAddresses": [],
+      "phoneNumbers": [{
+          "kind": "none",
+          "phoneNumber": "0745535785"
+      }],
+      "relations": [],
+      "users": [{
+          "id": "test-user-2",
+          "credentials": "test-user-1-credentials",
+          "firstName": "John 2",
+          "lastName": "Doe 2",
+          "addresses": [{
+            "street": "Street 2"
+          }]
+      }]
+      }
+                             """
+
   var authResponse: AuthenticationResponse = null;
   "PartiesApi" should "get all parties" in {
     Post("/users/authentication", HttpEntity(ContentTypes.`application/json`, userAuthRequestBodyJson)) ~> authenticationApiV1.route ~> check {
@@ -27,14 +63,25 @@ class PartiesApiTests extends FlatSpec with AssemblyTestBase with PartyApiV1Dire
       assert(body.toString.contains("authToken"))
       authResponse = JsonParser(body.asString).convertTo[AuthenticationResponse]
     }
-    Get("/parties")~>addHeader("Auth-Token", authResponse.authToken) ~> partiesApiV1.route ~> check{
-      val parties=JsonParser(body.asString).convertTo[List[Party]]
-      assert(parties.count(p=>true) == 1)
+    Get("/parties") ~> addHeader("Auth-Token", authResponse.authToken) ~> partiesApiV1.route ~> check {
+     val parties = JsonParser(body.asString).convertTo[List[Party]]
+      assert(parties.count(p => true) == 1)
       assert(parties(0).id.toString === "test-party-1")
     }
   }
 
-  "it" should "create a new party" is pending
+  "it" should "create and retrieve a new party" in {
+    Post("/users/authentication", HttpEntity(ContentTypes.`application/json`, userAuthRequestBodyJson)) ~> authenticationApiV1.route ~> check {
+      assert(status === StatusCodes.OK)
+      assert(body.toString.contains("authToken"))
+      authResponse = JsonParser(body.asString).convertTo[AuthenticationResponse]
+    }
+    Post("/parties", HttpEntity(ContentTypes.`application/json`, partyRequestBodyJson)) ~> addHeader("Auth-Token", authResponse.authToken) ~> partiesApiV1.route ~> check {
+      //assert(status === StatusCodes.OK)
+      //println(body.toString)
+    }
+    pending
+  }
 
   "it" should "get one party" is pending
 
