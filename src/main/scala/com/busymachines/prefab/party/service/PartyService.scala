fbc5667e9a3b1b0bd87297178762ddf379c2e88a
mateialexandru <alexandru.matei@busymachines.com>
2014-02-10 17:49:07 +0200

    added documentation for PartyService trait #18

--- src/main/scala/com/busymachines/prefab/party/service/PartyService.scala
+++ src/main/scala/com/busymachines/prefab/party/service/PartyService.scala
@@ -7,11 +7,70 @@ import com.busymachines.prefab.party.domain.User
 import com.busymachines.prefab.authentication.model.Credentials
 
 trait PartyService {
+
+  /**
+   * Creates or updates the credentials(loginName,password) for the given user
+   * @param userId - the user ID
+   * @param loginName - desired loginName
+   * @param password - desired password
+   * @return the new credentials
+   */
   def setLoginNamePassword(userId: Id[User], loginName: String, password: String): Future[Credentials]
+
+  /**
+   * Retrieves a party by ID
+   * @param partyId - the party ID
+   * @param sc - implicit SecurityContext
+   * @return
+   */
   def getParty(partyId: Id[Party])(implicit sc: SecurityContext): Future[Option[Party]]
+
+  /**
+   *Returns a list of all parties
+   * @param sc - implicit SecurityContext
+   * @return - list of parties
+   */
   def listParties(implicit sc: SecurityContext): Future[List[Party]]
+
+  /**
+   *Stores a given party into database
+   * @param party - the party to be stored
+   * @param sc - implicit SecurityContext
+   *@return - the stored party
+   */
   def createParty(party: Party)(implicit sc: SecurityContext): Future[Party]
+
+  /**
+   *Deletes a party by ID
+   * @param entityId - the party ID
+   * @param sc - implicit SecurityContext
+   * @return
+   */
   def deleteParty(entityId: Id[Party])(implicit sc: SecurityContext): Future[Unit]
+
+  /**
+   *Updates a user by ID
+   * @param id - the user ID
+   * @param user - the updated user
+   *@param sc - implicit SecurityContext
+   *@return
+   */
   def updateUser(id: Id[User], user: User)(implicit sc: SecurityContext): Future[Unit]
+
+  /**
+   *Retrieves a user by ID
+   * @param id - the user ID
+   * @param sc - implicit SecurityContext
+   * @return - the user, if it exists
+   */
   def findUser(id: Id[User])(implicit sc: SecurityContext): Future[Option[User]]
+
+  /**
+   * To check if user has enough rights to use a specific party id for specific operations (eg. to create a location for this partyId) we have to
+   * check if that party is the party of current user OR if it's a child party.
+   * @param partyId - the party ID
+   * @param user - the user ID
+   * @return - true/false
+   */
+  def userHasEnoughRights(partyId: Id[Party], user: User): Future[Boolean]
 }
