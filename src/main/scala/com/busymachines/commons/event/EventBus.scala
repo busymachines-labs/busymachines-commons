8b9b62d27a14f40054e4a3fff2e1ffcdcbc529f4
Cristi Popovici <popovici@eloquentix.com>
2014-03-11 23:00:54 +0200

    removed unused import statements

--- src/main/scala/com/busymachines/commons/event/EventBus.scala
+++ src/main/scala/com/busymachines/commons/event/EventBus.scala
@@ -1,11 +1,5 @@
 package com.busymachines.commons.event
 
-import akka.actor.ActorRef
-import akka.actor.Actor
-import scala.concurrent.Future
-import com.busymachines.commons.domain.Id
-import scala.reflect.ClassTag
-import java.util.UUID
 
 trait BusEvent
 
