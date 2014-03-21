3b38d576e046187a9ee258384b5a7ff81c2e9319
Ruud Diterwich <ruud@diterwich.com>
2014-03-21 07:31:17 +0100

    fixed taxe import

--- src/main/scala/com/busymachines/commons/CsvParser.scala
+++ src/main/scala/com/busymachines/commons/CsvParser.scala
@@ -14,42 +14,46 @@ object CsvParser {
   
   def parse(reader : Reader, separator: Char = ',') : Array[Array[String]] = {
     val bufferedReader = new BufferedReader(reader)
-    val result = ArrayBuffer[Array[String]]()
-    var currentLine = ArrayBuffer[String]()
-    val currentField = new StringBuilder
-    var current = bufferedReader.read
-    var peek = bufferedReader.read
-    def goNext() { current = peek; peek = bufferedReader.read; if (current == '\r') if (peek == '\n') goNext() else current = '\n' }
-    def skipWhitespace() = while (current == ' ' || current == '\t') goNext()
-    while (current != -1) {
-      currentLine.clear()
-      skipWhitespace()
-      while (current != '\n' && current != -1) {
-        currentField.clear()
-        if (current == '"') {
-          goNext()
-          while (current != -1 && (current != '"' || peek == '"')) {
-            if (current == '"') goNext()
-            currentField append current.asInstanceOf[Char]
-            goNext()
-          }
-          if (current == '"') goNext()
-          skipWhitespace()
-        }
-        else {
-          while (current != -1 && current != separator && current != '\n') {
-            currentField append current.asInstanceOf[Char]
-            goNext()
-          }
-        }
-        if (current == separator) goNext()
+    try {
+      val result = ArrayBuffer[Array[String]]()
+      var currentLine = ArrayBuffer[String]()
+      val currentField = new StringBuilder
+      var current = bufferedReader.read
+      var peek = bufferedReader.read
+      def goNext() { current = peek; peek = bufferedReader.read; if (current == '\r') if (peek == '\n') goNext() else current = '\n' }
+      def skipWhitespace() = while (current == ' ' || current == '\t') goNext()
+      while (current != -1) {
+        currentLine.clear()
         skipWhitespace()
-        currentLine += currentField.toString.trim
+        while (current != '\n' && current != -1) {
+          currentField.clear()
+          if (current == '"') {
+            goNext()
+            while (current != -1 && (current != '"' || peek == '"')) {
+              if (current == '"') goNext()
+              currentField append current.asInstanceOf[Char]
+              goNext()
+            }
+            if (current == '"') goNext()
+            skipWhitespace()
+          }
+          else {
+            while (current != -1 && current != separator && current != '\n') {
+              currentField append current.asInstanceOf[Char]
+              goNext()
+            }
+          }
+          if (current == separator) goNext()
+          skipWhitespace()
+          currentLine += currentField.toString.trim
+        }
+        if (current == '\n') goNext()
+        if (!currentLine.isEmpty)
+          result += currentLine.toArray
       }
-      if (current == '\n') goNext()
-      if (!currentLine.isEmpty) 
-        result += currentLine.toArray
+      result.toArray
+    } finally {
+      bufferedReader.close()
     }
-    result.toArray
   }
 }
