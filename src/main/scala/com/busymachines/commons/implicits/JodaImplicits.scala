4dee98c2a202d1cee54f5bc7d2d3bd3afd2eebef
Ruud Diterwich <ruud@diterwich.com>
2014-05-16 08:28:39 +0200

    Simplified JodaImplicits

--- src/main/scala/com/busymachines/commons/implicits/JodaImplicits.scala
+++ src/main/scala/com/busymachines/commons/implicits/JodaImplicits.scala
@@ -28,11 +28,10 @@ trait JodaImplicits { this: CommonJsonFormats =>
   implicit val jodaTimeZoneFormat = stringFormat("JodaTimeZone", s => DateTimeZone.forID(s))
   
   implicit val jodaLocalDateFormat = new RootJsonFormat[LocalDate] {
-    val format = ISODateTimeFormat.date
-    def write(value: LocalDate) = JsString(format.print(value))
+    def write(value: LocalDate) = JsString(value.toString)
     def read(value: JsValue): LocalDate = value match {
       case JsString(s) =>
-        try LocalDate.parse(stripTimeZone(stripTime(s)))
+        try DateTime.parse(s).toLocalDate
         catch {
           case e: Throwable => deserializationError("Couldn't convert '" + s + "' to a local date: " + e.getMessage)
         }
@@ -41,39 +40,22 @@ trait JodaImplicits { this: CommonJsonFormats =>
   }
 
   implicit val jodaLocalDateTimeFormat = new RootJsonFormat[LocalDateTime] {
-    val format = ISODateTimeFormat.dateTime
-    def write(value: LocalDateTime) = JsString(format.print(value))
+    def write(value: LocalDateTime) = JsString(value.toString)
     def read(value: JsValue): LocalDateTime = value match {
       case JsString(s) =>
-        try LocalDateTime.parse(stripTimeZone(s))
+        try DateTime.parse(s).toLocalDateTime
         catch {
           case e: Throwable => deserializationError("Couldn't convert '" + s + "' to a local date-time: " + e.getMessage)
         }
       case s => deserializationError("Couldn't convert '" + s + "' to a local date-time")
     }
   }
-
-  private def stripTimeZone(s: String): String = 
-    if (s.endsWith("Z")) s.dropRight(1)
-    else s.lastIndexOf('+') match {
-      case -1 => s
-      case i => s.substring(0, i)
-    }
   
-  private def stripTime(s: String): String = 
-    s.lastIndexOf('T') match {
-      case -1 => s
-      case i => s.substring(0, i)
-    }
-  
-  val jodaDateTimeParser = ISODateTimeFormat.dateOptionalTimeParser.withZoneUTC
-  val jodaDateTimeParserFormatter = ISODateTimeFormat.dateTime.withZoneUTC
-
   implicit val jodaDateTimeFormat = new RootJsonFormat[DateTime] {
-    def write(value: DateTime) = JsString(jodaDateTimeParserFormatter.print(value))
+    def write(value: DateTime) = JsString(value.toString())
     def read(value: JsValue): DateTime = value match {
       case JsString(s) =>
-        try jodaDateTimeParser.parseDateTime(s)
+        try DateTime.parse(s)
         catch {
           case e: Throwable => deserializationError("Couldn't convert '" + s + "' to a date-time: " + e.getMessage)
         }
