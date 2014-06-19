51bbf8d6d83c527768d77896ba974ff137cdd687
Ruud Diterwich <ruud@diterwich.com>
2014-06-19 10:07:56 +0200

    Moved installer to commons

---
+++ src/main/scala/com/busymachines/commons/installer/InstallCommand.scala
@@ -0,0 +1,110 @@
+package com.busymachines.commons.installer
+
+import java.io.{File, FileWriter}
+import java.net.{URL, URLClassLoader}
+import java.nio.file.attribute.{PosixFilePermission, UserPrincipal}
+import java.nio.file.{FileSystems, Files}
+
+import com.busymachines.commons.Implicits._
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable.HashSet
+import scala.io.Source
+
+object InstallCommand {
+  
+  val genmarker = "### This file has been generated ###"
+
+  def install(name: String, description: String, version: String, user: Option[String], vmArgs: String, app: App, args: String) = {
+    if (new File("/etc/init.d").isDirectory) {
+      InstallOnUbuntuCommand.install(name, description, version, user, vmArgs, app, args)
+    }
+    else if (new File("/Library/LaunchDaemons").isDirectory) {
+      InstallOnUbuntuCommand.install(name, description, version, user, vmArgs, app, args)
+//      InstallOnOsxCommand.install(name, description, user)
+    }
+    else {
+      throw new Exception("Couldn't install application: unrecognized environment")
+    }
+  }
+
+  def getUserPrincipal(user: Option[String]) = {
+    val principalLookupService = FileSystems.getDefault.getUserPrincipalLookupService
+    try user.map(principalLookupService.lookupPrincipalByName)
+    catch {
+      case e: Exception => throw new Exception("user '" + user.get + "' does not exist")
+    }
+  }
+
+  def setOwner(file: File, user: Option[UserPrincipal]) =
+    if (user.isDefined)
+      try Files.setOwner(file.toPath, user.get)
+      catch { case e: Exception => throw new Exception("couldn't set owner of " + file + ": " + e.getMessage) }
+
+  def setPermissions(file: File, permissions: PosixFilePermission*) =
+    try Files.setPosixFilePermissions(file.toPath, Set(permissions:_*))
+    catch { case e: Exception => throw new Exception("couldn't set permissions of " + file + ": " + e.getMessage) }
+
+  def copyJars(dest: File): List[String] = {
+    var bootEntries = new HashSet[URL]
+    ClassLoader.getSystemClassLoader.getParent match {
+      case cl: URLClassLoader =>
+//        for (entry <- cl.getURLs) println("boot entry: " + entry)
+        bootEntries ++= cl.getURLs
+      case _ =>
+    }
+    getClass.getClassLoader match {
+      case cl: URLClassLoader =>
+        for {
+          url <- cl.getURLs.toList if !bootEntries.contains(url)
+          fileName = url.fileName if !url.toString.contains("/jre/lib/")
+          file = new File(dest, fileName)
+          _ = println(s"Copying $file")
+          _ = if (!file.exists || file.isFile) {
+            dest.mkdirs()
+            url.copyTo(new File(dest, fileName))
+          }
+        } yield fileName
+      case _ => Nil
+    }
+  }
+
+  // Copy application.conf
+  def writeApplicationConf(dest: File) {
+    if (!dest.exists) {
+      dest.getParentFile.mkdirs()
+      val writer = new FileWriter(dest)
+      try {
+        for (line <- Source.fromURL(getClass.getClassLoader.getResource("application.conf")).getLines()) {
+          if (line.trim.nonEmpty && !line.trim.startsWith("#"))
+            writer.append("#")
+          writer.append(line).append("\n")
+        }
+        println(s"Created $dest")
+      } finally {
+        writer.close()
+      }
+    } else {
+      println(s"Kept existing file $dest")
+    }
+  }
+
+  def writeResource(resource: String, dest: File) {
+    if (!dest.exists) {
+      dest.getParentFile.mkdirs()
+      getClass.getClassLoader.getResource(resource).copyTo(dest)
+      println(s"Created $dest")
+    } else {
+      println(s"Kept existing file $dest")
+    }
+  }
+
+  def canOverwrite(file: File) =
+    !file.exists || hasGenMarker(file)
+    
+  def hasGenMarker(file: File) = 
+    file.isFile && Source.fromFile(file).getLines().exists(_.contains(genmarker))
+
+}
+
+
---
+++ src/main/scala/com/busymachines/commons/installer/InstallOnUbuntuCommand.scala
@@ -0,0 +1,165 @@
+package com.busymachines.commons.installer
+
+import java.io.File
+import java.nio.file.attribute.PosixFilePermission._
+
+import com.busymachines.commons.Implicits._
+
+object InstallOnUbuntuCommand {
+  import com.busymachines.commons.installer.InstallCommand._
+
+  def install(name: String, description: String, version: String, user: Option[String], vmArgs: String, app: App, args: String) {
+    try install2(name, description, version, user, vmArgs, app, args)
+    catch { 
+      case e: Exception =>
+        println("Error: " + e.getMessage)
+      }
+    }
+  
+  def install2(name: String, description: String, version: String, userName: Option[String], vmArgs: String, app: App, args: String) {
+    println(s"Installing $name")
+    
+    // check user principal
+    val user = getUserPrincipal(userName)
+
+    // make sure we have root rights
+    val libDir = new File("/var/lib/" + name)
+    libDir.mkdirs()
+    setOwner(libDir, user)
+
+    // copy jars into lib
+    val jars = copyJars(libDir)
+    val jarFileNames = jars.map(new File(libDir, _).getAbsolutePath).mkString(",")
+    val appClassName = app.getClass.getName.stripSuffix("$")
+
+
+    // write start script
+    val startScript = new File("/usr/bin/" + name)
+    if (canOverwrite(startScript)) {
+      startScript.getParentFile.mkdirs()
+
+      val startScriptContent = s"""#!/bin/sh
+$genmarker
+java $vmArgs -Dconfig.file=/etc/$name.conf -Dlogback.configurationFile=/etc/$name.d/cli-logback.xml -cp $jarFileNames $appClassName $$*
+"""
+
+      startScriptContent.copyTo(startScript)
+      setPermissions(startScript, OWNER_EXECUTE, OWNER_READ, OWNER_WRITE, GROUP_EXECUTE, GROUP_READ, OTHERS_EXECUTE, OTHERS_READ)
+      println("Created " + startScript)
+    } else {
+      println("Warning: existing file not overwritten: " + startScript)
+    }
+
+    // write init script
+    val initScript = new File("/etc/init.d/", name)
+    if (canOverwrite(initScript)) {
+      initScript.getParentFile.mkdirs()
+      try {
+
+        val command = s"java $vmArgs -Dconfig.file=/etc/$name.conf -Dlogback.configurationFile=/etc/$name.d/logback.xml -cp $jarFileNames $appClassName $args"
+
+        val initScriptContent = mkInitScript(name, description, command, "", userName.getOrElse("root"))
+        initScriptContent.copyTo(initScript)
+        setPermissions(initScript, OWNER_EXECUTE, OWNER_READ, OWNER_WRITE, GROUP_EXECUTE, GROUP_READ, OTHERS_EXECUTE, OTHERS_READ)
+        println("Created " + initScript)
+      } catch {
+        case e: Exception => println("Warning: couldn't create: " + initScript + ": " + e.getMessage)
+      }
+    } else {
+      println("Warning: existing file not overwritten: " + initScript)
+    }
+
+    // write application.conf
+    writeApplicationConf(new File("/etc/" + name + ".conf"))
+
+    // write logback.xml
+    writeResource("logback.xml", new File("/etc/" + name + ".d/logback.xml"))
+    writeResource("cli-logback.xml", new File("/etc/" + name + ".d/cli-logback.xml"))
+
+    // change owner of log file
+    val logFile = new File(s"/var/log/$name.log")
+    if (!logFile.exists()) "".copyTo(logFile)
+    setOwner(logFile, user)
+  }
+  
+  def mkInitScript(name: String, description: String, command: String, args: String, user: String) =
+    initScriptTemplate.
+      replace("<GEN_MARKER>", genmarker).
+      replaceAll("<NAME>", name).
+      replaceAll("<DESCRIPTION>", description).
+      replaceAll("<COMMAND>", command).
+      replaceAll("<USER>", user).
+      replace("<ARGS>", args)
+
+
+  val initScriptTemplate = """#!/bin/sh
+<GEN_MARKER>
+### BEGIN INIT INFO
+# Provides:          <NAME>
+# Required-Start:    cassandra elasticsearch networking
+# Required-Stop:     cassandra elasticsearch networking
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Description:       <DESCRIPTION>
+### END INIT INFO
+
+SCRIPT="<COMMAND> <ARGS>"
+RUNAS=<USER>
+
+PIDFILE=/var/run/<NAME>.pid
+LOGFILE=/var/log/<NAME>.log
+
+start() {
+  if [ -f $PIDFILE ] && kill -0 $(cat $PIDFILE); then
+    echo 'Service already running' >&2
+    return 1
+  fi
+  echo 'Starting <NAME>...' >&2
+  local CMD="$SCRIPT &> \"$LOGFILE\" & echo \$!"
+  su -c "$CMD" $RUNAS > "$PIDFILE"
+  echo 'Service started' >&2
+}
+
+stop() {
+  if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
+    echo '<NAME> not running' >&2
+    return 1
+  fi
+  echo 'Stopping <NAME>...' >&2
+  kill -15 $(cat "$PIDFILE") && rm -f "$PIDFILE"
+  echo 'Service stopped' >&2
+}
+
+uninstall() {
+  echo -n "Are you really sure you want to uninstall <NAME>? That cannot be undone. [yes|No] "
+  local SURE
+  read SURE
+  if [ "$SURE" = "yes" ]; then
+    stop
+    rm -f "$PIDFILE"
+    echo "Notice: log file is not be removed: '$LOGFILE'" >&2
+    update-rc.d -f <NAME> remove
+    rm -fv "$0"
+  fi
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  uninstall)
+    uninstall
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|uninstall}"
+esac
+"""
+
+}
