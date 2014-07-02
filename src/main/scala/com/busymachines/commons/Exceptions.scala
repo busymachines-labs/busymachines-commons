9593bb9b5cf3757afe10b45217dab41c4bb0d55b
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-07-02 12:06:53 +0300

    Added EntityNotFoundException and NotAuthorizedException to the common handler

--- src/main/scala/com/busymachines/commons/Exceptions.scala
+++ src/main/scala/com/busymachines/commons/Exceptions.scala
@@ -2,15 +2,15 @@ package com.busymachines.commons
 
 import com.busymachines.commons.domain.Id
 
-class NotAuthenticatedException(msg : String, cause : Option[Throwable] = None) extends Exception(msg, cause.getOrElse(null))
+class NotAuthenticatedException(msg: String, cause: Option[Throwable] = None) extends Exception(msg, cause.getOrElse(null))
 
-class NotAuthorizedException(msg:String, cause: Option[Throwable] = None) extends Exception(msg, cause.getOrElse(null))
+class NotAuthorizedException(msg: String, cause: Option[Throwable] = None) extends Exception(msg, cause.getOrElse(null))
 
 /**
  * Thrown when a specified id was not valid. The message should be descriptive, contain the , english and is meant for end-user
  * consumption.
  */
-class EntityNotFoundException(val id : String, val `type` : String) extends Exception(s"${`type`.capitalize} with id $id not found") {
-  def this(id : Id[_], `type` : String) = this(id.toString, `type`)
+class EntityNotFoundException(val id: String, val `type`: String) extends Exception(s"${`type`.capitalize} with id $id not found") {
+  def this(id: Id[_], `type`: String) = this(id.toString, `type`)
 }
 
--- src/main/scala/com/busymachines/commons/spray/CommonExceptionHandler.scala
+++ src/main/scala/com/busymachines/commons/spray/CommonExceptionHandler.scala
@@ -1,6 +1,6 @@
 package com.busymachines.commons.spray
 
-import com.busymachines.commons.Logging
+import com.busymachines.commons.{NotAuthorizedException, EntityNotFoundException, Logging}
 import spray.http.StatusCodes
 import spray.routing.{Route, ExceptionHandler}
 
@@ -16,6 +16,13 @@ object CommonExceptionHandler extends ExceptionHandler with Logging {
   override def isDefinedAt(x: Throwable): Boolean = true
 
   override def apply(v1: Throwable): Route = v1 match {
+    case e: EntityNotFoundException => ctx =>
+      ctx.complete (StatusCodes.NotFound, Map ("message" -> e.getMessage, "id" -> e.id, "type" -> e.`type`))
+
+    case e: NotAuthorizedException => ctx =>
+      debug (s"Exception handler rejecting with forbidden because exception : $e")
+      ctx.complete (StatusCodes.Forbidden)
+
     case e: Throwable => ctx =>
       error(s"Request ${ctx.request} could not be handled normally: ${e.getMessage}", e)
       ctx.complete(StatusCodes.InternalServerError, Map("message" -> e.getMessage))
