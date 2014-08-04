2267ea48d29a6eaa27f6d1e5097f8659e3a1ddd5
Ruud Diterwich <ruud@diterwich.com>
2014-08-04 17:44:39 +0200

    Removed polling loop taking up 100% CPU

--- src/main/scala/com/busymachines/commons/elasticsearch/tools/ESAutoImport.scala
+++ src/main/scala/com/busymachines/commons/elasticsearch/tools/ESAutoImport.scala
@@ -2,29 +2,29 @@ package com.busymachines.commons.elasticsearch.tools
 
 import java.io.File
 
+import akka.actor.Scheduler
 import com.busymachines.commons.Logging
 import com.busymachines.commons.elasticsearch.{ESMapping, ESConfig, ESIndex}
 import spray.json.JsObject
 
 import scala.collection.mutable
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
-class ESAutoImport(config: ESConfig, index: String, files: Seq[File], mappings: String => Option[ESMapping[_]])(implicit ec: ExecutionContext) extends Logging {
+class ESAutoImport(config: ESConfig, index: String, files: Seq[File], mappings: String => Option[ESMapping[_]])(implicit ec: ExecutionContext, scheduler: Scheduler) extends Logging {
 
   private val lastModifiedMap = new mutable.HashMap[File, Long]
 
-  val future = Future {
-    while (true) {
-      for (file <- files) {
-        val lastModified = if (file.exists) file.lastModified else -1
-        if (lastModifiedMap.put(file, lastModified) != Some(lastModified)) {
-          if (lastModified != -1) {
-            info(s"Importing file $file")
-            try {
-              ESImport.importJson(config, index, file, overwrite = true, dryRun = false, mappings, (obj: JsObject) => {})
-            } catch {
-              case t: Throwable => logger.error(t)
-            }
+  scheduler.schedule(0.seconds, 1.seconds) {
+    for (file <- files) {
+      val lastModified = if (file.exists) file.lastModified else -1
+      if (lastModifiedMap.put(file, lastModified) != Some(lastModified)) {
+        if (lastModified != -1) {
+          info(s"Importing file $file")
+          try {
+            ESImport.importJson(config, index, file, overwrite = true, dryRun = false, mappings, (obj: JsObject) => {})
+          } catch {
+            case t: Throwable => logger.error(t)
           }
         }
       }
