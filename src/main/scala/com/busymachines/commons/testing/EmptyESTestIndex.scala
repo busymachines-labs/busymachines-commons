34bfcfa93d2c4afde0705ada1f136030d2f1e7d9
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-08-07 14:17:38 +0300

    Added waiting for shards in the EmptyESTestIndex

--- src/main/scala/com/busymachines/commons/testing/EmptyESTestIndex.scala
+++ src/main/scala/com/busymachines/commons/testing/EmptyESTestIndex.scala
@@ -1,25 +1,32 @@
 package com.busymachines.commons.testing
 
 import scala.collection.concurrent
-import com.busymachines.commons.elasticsearch.ESConfig
-import com.busymachines.commons.elasticsearch.ESIndex
+
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest
+import org.elasticsearch.node.NodeBuilder
+
+import com.busymachines.commons.elasticsearch.{ ESConfig, ESIndex }
 import com.busymachines.commons.event.{ DoNothingEventSystem, EventBus }
-import org.elasticsearch.action.NoShardAvailableActionException
 
 object EmptyESTestIndex {
   private val usedIndexes = concurrent.TrieMap[String, Int]()
-
   def getNextName(baseName: String): String = {
     val i = usedIndexes.getOrElse(baseName, 0)
     usedIndexes(baseName) = i + 1
     baseName + (if (i > 0) i else "")
   }
+  lazy val node = NodeBuilder.nodeBuilder().local(true).data(true).node();
 }
 
 class EmptyESTestIndex(c: Class[_], config: ESConfig = DefaultTestESConfig, eventBus: EventBus = DoNothingEventSystem)
-  extends ESIndex(config, EmptyESTestIndex.getNextName("test-" + c.getName.toLowerCase), eventBus) {
+  extends ESIndex(config, EmptyESTestIndex.getNextName("test-" + c.getSimpleName.toLowerCase), eventBus) {
 
   try {
+
+    EmptyESTestIndex.node.client().admin().cluster().health(new ClusterHealthRequest(indexName).waitForActiveShards(1)).actionGet();
+    //    client.javaClient.admin().cluster().prepareHealth().setWaitForGreenStatus().execute().actionGet();
+    //    val deleteQuery = org.elasticsearch.client.Requests.delete("_all")
+    //    client.javaClient.admin().indices().delete(deleteQuery)
     drop()
   } catch {
     case e: Throwable => {
