b616a9313e2813147b36ada095acc18b0bd7cf38
Ruud Diterwich <ruud@diterwich.com>
2014-08-20 16:27:27 +0200

    Logging changes

--- src/main/scala/com/busymachines/commons/logger/Logging.scala
+++ src/main/scala/com/busymachines/commons/logger/Logging.scala
@@ -3,11 +3,11 @@ package com.busymachines.commons.logger
 import org.apache.logging.log4j.{Level, LogManager}
 
 trait AdditionalParameters {
-  def apply(originalParameters: Map[String, String]): Map[String, String]
+  def apply: Seq[(String, String)]
 }
 
 object DefaultAdditionalParameters extends AdditionalParameters {
-  def apply(originalParameters: Map[String, String]) = originalParameters
+  def apply: Seq[(String, String)] = Nil
 }
 
 trait Logging {
@@ -28,42 +28,42 @@ sealed class Logger {
   def trace(message: => String, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
     log(Level.TRACE, message, None, parameters)
 
-  def trace(message: => String, e: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
-    log(Level.TRACE, message, Some(e), parameters)
+  def trace(message: => String, cause: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
+    log(Level.TRACE, message, Some(cause), parameters)
 
   def debug(message: => String, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
     log(Level.DEBUG, message, None, parameters)
 
-  def debug(message: => String, e: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
-    log(Level.DEBUG, message, Some(e), parameters)
+  def debug(message: => String, cause: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
+    log(Level.DEBUG, message, Some(cause), parameters)
 
   def info(message: => String, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
     log(Level.INFO, message, None, parameters)
 
-  def info(message: => String, e: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
-    log(Level.INFO, message, Some(e), parameters)
+  def info(message: => String, cause: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
+    log(Level.INFO, message, Some(cause), parameters)
 
   def warn(message: => String, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
     log(Level.WARN, message, None, parameters)
 
-  def warn(message: => String, e: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
-    log(Level.WARN, message, Some(e), parameters)
+  def warn(message: => String, cause: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
+    log(Level.WARN, message, Some(cause), parameters)
 
   def error(message: => String, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
     log(Level.ERROR, message, None, parameters)
 
-  def error(message: => String, e: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
-    log(Level.ERROR, message, Some(e), parameters)
+  def error(message: => String, cause: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
+    log(Level.ERROR, message, Some(cause), parameters)
 
   def fatal(message: => String, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
     log(Level.FATAL, message, None, parameters)
 
-  def fatal(message: => String, e: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
-    log(Level.FATAL, message, Some(e), parameters)
+  def fatal(message: => String, cause: => Throwable, parameters: (String, String)*)(implicit ap: AdditionalParameters) =
+    log(Level.FATAL, message, Some(cause), parameters)
 
   private def log(level: Level, message: => String, cause: Option[Throwable], parameters: Seq[(String, String)])(implicit ap: AdditionalParameters) {
     if (logger.isEnabled(level)) {
-      val commonsLogMessage = CommonsLoggerMessage(message, cause.orNull, ap(parameters.toMap))
+      val commonsLogMessage = CommonsLoggerMessage(message, cause.orNull, (parameters ++ ap.apply).toMap)
       logger.log(level, commonsLogMessage, cause.orNull)
     }
   }
