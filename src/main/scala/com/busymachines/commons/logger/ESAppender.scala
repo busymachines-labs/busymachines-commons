599cfe1d19e93950011681e125214ef9ba7e83f0
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-08-20 12:30:16 +0300

    [#2198] Added multiple lifecycle improvements.
      * added a flush() method that empties the queue. This should be called only upon termination of the program because it won't stop until the logMessage queue is empty.
      * the background process now sleeps for one second if there aren't enough messages in the queue. If, after it wakes up there still aren't enough, it just writes the ones it has into ES.
      * overriden the `stop` method.
        * it sets an atomic boolean flag that the process polls, indicating it to stop.
        * it flushes whatever is left in the message queue.
    
    Unfortunately it's evil imperative java code.

--- src/main/scala/com/busymachines/commons/logger/ESAppender.scala
+++ src/main/scala/com/busymachines/commons/logger/ESAppender.scala
@@ -1,13 +1,11 @@
 package com.busymachines.commons.logger.appender
 
 import java.util.concurrent.LinkedBlockingQueue
-
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
-
 import org.apache.logging.log4j.core.{ Filter, Layout, LogEvent }
 import org.apache.logging.log4j.core.appender.AbstractAppender
 import org.apache.logging.log4j.core.config.plugins.{ Plugin, PluginAttribute, PluginElement, PluginFactory }
@@ -20,10 +18,11 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 import org.scalastuff.esclient.ActionMagnet
-
 import com.busymachines.commons.elasticsearch.ESConfig
 import com.busymachines.commons.logger.ESLayout
 import com.busymachines.commons.logger.domain.{ ExceptionLogJsonFormats, LogMessage }
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * Created by Alexandru Matei on 14.08.2014.
@@ -70,22 +69,12 @@ class ESAppender(
   }
 
   lazy val actualIndexName = s"$indexNamePrefix.${DateTimeFormat.forPattern(indexNameDateFormat).print(DateTime.now)}"
+  val typeName = "log"
 
   lazy val javaClient = {
     new TransportClient(ImmutableSettings.settingsBuilder().put("cluster.name", config.clusterName)) {
       addTransportAddresses(config.hostNames.map(new InetSocketTransportAddress(_, config.port)): _*)
     }
-  }
-
-  val typeName = "log"
-
-  import ExceptionLogJsonFormats._
-  def bulk(list: Seq[LogMessage]): Unit = {
-    val bulkRequest = javaClient.prepareBulk()
-    list.foreach(o => bulkRequest.add(javaClient.
-      prepareIndex(actualIndexName, typeName).
-      setSource(logMessageFormat.write(o).toString)))
-    bulkRequest.execute()
   }
 
   def initIndex() {
@@ -110,25 +99,92 @@ class ESAppender(
       createIndex(actualIndexName)
   }
 
-  lazy val messageQueue = new LinkedBlockingQueue[LogMessage](queueSize)
+  import ExceptionLogJsonFormats._
+  def bulk(list: Seq[LogMessage]): Unit = {
+    val bulkRequest = javaClient.prepareBulk()
+    list.foreach(o => bulkRequest.add(javaClient.
+      prepareIndex(actualIndexName, typeName).
+      setSource(logMessageFormat.write(o).toString)))
+    bulkRequest.execute()
+  }
+
+  private val messageQueue = new LinkedBlockingQueue[LogMessage](queueSize)
+
+  /**
+   * This is basically Java right here. Looks horrible, don't judge.
+   *
+   * Used when the system is shutting down. Flushes anything that's left in the
+   * message queue.
+   */
+  private def flush() {
+    import scala.util.control.Breaks._
+    var temp: LogMessage = null
+    val listBuffer = ListBuffer[LogMessage]()
+    while (!messageQueue.isEmpty()) {
+      try {
+        //just because at the while loop is wasn't empty doesn't mean
+        //that by this time there actually is anything in the queue.
+        //We do not have a guarantee that the process future has stopped executing
+        //when we run this thing.
+        temp = messageQueue.poll(1, TimeUnit.SECONDS)
+        if (temp == null) {
+          break
+        } else {
+          listBuffer += temp
+        }
+      } catch {
+        case e: Throwable => break
+      }
+    }
+    try {
+      bulk(listBuffer)
+    } catch {
+      case e: Throwable => //ignore
+    }
+  }
+
+  val isInterrupted = new AtomicBoolean(false)
   import scala.concurrent.ExecutionContext.Implicits.global
 
+  def takeMultipleLogMessage(number: Int): ListBuffer[LogMessage] = {
+    val listBuffer = ListBuffer[LogMessage]()
+    (0 until number).foreach(i => {
+      listBuffer += messageQueue.take()
+    })
+    listBuffer
+  }
+
   val process = Future {
+    import scala.util.control.Breaks._
     initIndex()
-    while (true) {
+    while (!isInterrupted.get()) {
       if (messageQueue.size() >= bulkSize) {
         try {
-          val listBuffer: ListBuffer[LogMessage] = ListBuffer[LogMessage]()
-          (0 until bulkSize).foreach(i => {
-            listBuffer += messageQueue.take()
-          })
-
+          val listBuffer = takeMultipleLogMessage(bulkSize)
           bulk(listBuffer)
         } catch {
           case ex: Exception => println(ex)
         }
+      } else {
+        try {
+          Thread.sleep(1000)
+          if (messageQueue.size() >= bulkSize) {
+            //if it's larger we just continue the normal flow of the application
+          } else {
+            val listBuffer = takeMultipleLogMessage(messageQueue.size())
+            bulk(listBuffer)
+          }
+        } catch {
+          case e: Throwable => if (isInterrupted.get()) break
+        }
       }
     }
+  }
+
+  override def stop() {
+    isInterrupted.set(true)
+    super.stop()
+    flush()
   }
 
   override def append(event: LogEvent): Unit = {
