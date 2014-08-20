539ee36b63da9aa95cc707ad6f9a0fc27654c2da
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-08-20 19:25:28 +0300

    [#2198] Added logging back to DoNothingEventSystem

--- src/main/scala/com/busymachines/commons/event/DoNothingEventSystem.scala
+++ src/main/scala/com/busymachines/commons/event/DoNothingEventSystem.scala
@@ -1,16 +1,15 @@
 package com.busymachines.commons.event
 
 import com.busymachines.commons.logging.Logging
-import akka.actor.ActorRef
-import scala.concurrent.Future
 
-object DoNothingEventSystem extends EventBus{
+object DoNothingEventSystem extends EventBus with Logging {
 
   def subscribe(f: PartialFunction[BusEvent, Any]): Unit = {
-//    debug(s"Subscribed to endpoint but in fact did nothing")
+    logger.debug(s"Subscribed to endpoint but in fact did nothing")
   }
 
-  def publish(event: BusEvent): Unit = {}
-//    debug(s"Published event $event")
+  def publish(event: BusEvent): Unit = {
+    logger.debug(s"Published event $event")
+  }
 
 }
