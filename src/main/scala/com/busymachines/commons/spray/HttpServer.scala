c7c8fd2203a159dee304329e5c382d97a238cc72
Paul Sabou <paul.sabou@gmail.com>
2014-08-27 13:48:28 +0200

    added monitor at hhtp level

--- src/main/scala/com/busymachines/commons/spray/HttpServer.scala
+++ src/main/scala/com/busymachines/commons/spray/HttpServer.scala
@@ -12,13 +12,8 @@ import _root_.spray.http.HttpRequest
 import _root_.spray.http.StatusCodes
 import _root_.spray.routing.Directive.pimpApply
 import _root_.spray.routing.HttpService.pimpRouteWithConcatenation
-import _root_.spray.routing.HttpServiceActor
-import _root_.spray.routing.Route
-import _root_.spray.routing.RoutingSettings
+import spray.routing._
 import _root_.spray.routing.directives.LogEntry
-import _root_.spray.routing.ExceptionHandler
-import _root_.spray.routing.HttpService
-import _root_.spray.routing.RejectionHandler
 import com.busymachines.commons.Implicits._
 import _root_.spray.http.IllegalRequestException
 import scala.util.control.NonFatal
@@ -26,14 +21,15 @@ import _root_.spray.util.LoggingContext
 import _root_.spray.http._
 import _root_.spray.json._
 import StatusCodes._
-import _root_.spray.routing.AuthorizationFailedRejection
+import com.netaporter.salad.metrics.spray.metrics.{MetricHelpers, MetricsDirectiveFactory}
+import com.netaporter.salad.metrics.spray.routing.directives.BasicDirectives._
 
 case class HttpServerConfig(baseName: String) extends CommonConfig(baseName) {
   val interface = string("interface")
   val port = int("port")
 }
 
-abstract class HttpServer(config: HttpServerConfig)(implicit actorSystem : ActorSystem) extends CommonHttpService with CORSDirectives{
+abstract class HttpServer(config: HttpServerConfig,metricsDirectiveFactory: MetricsDirectiveFactory)(implicit actorSystem : ActorSystem) extends CommonHttpService with CORSDirectives{
 
   val serverName : String = "http-server"
   val routingSettings = RoutingSettings(actorSystem)
@@ -44,8 +40,16 @@ abstract class HttpServer(config: HttpServerConfig)(implicit actorSystem : Actor
 
   class Actor extends HttpServiceActor {
     val theRoute = route
-    def receive = runRoute(logRequest(showRequest _) { theRoute })
+
+    def receive = runRoute(logRequest(showRequest _) {
+      monitorRequest(null,theRoute) })
+
     def showRequest(request: HttpRequest) = LogEntry("URL: " + request.uri + "\n CONTENT: " + request.entity, DebugLevel)
+    def monitorRequest(ctx:RequestContext,route:Route):Route =
+    {
+      (metricsDirectiveFactory.counter("aurum").all.count &
+        metricsDirectiveFactory.timer("aurum").time)(route)
+    }
   }
 
   def start() =
