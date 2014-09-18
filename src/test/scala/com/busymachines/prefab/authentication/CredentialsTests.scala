3569555197d7875e5edcc3a8b282a6671f7b2cce
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-09-18 16:36:39 +0300

    [#2326] Security update, version bumped to 0.6.1. Password credentials are now hased using SHA256.
    * If a client project wishes to upgrade to version 0.6.1 it will have to:
        * pass an extra parameters HashFunction parameter to `CredentialsAuthenticator`. Predefined functions can be found
          in `com.busymachines.prefab.authentication.model.HashFunctions`
        * slowly start requesting password resets and hashing the new ones using SHA256

--- src/test/scala/com/busymachines/prefab/authentication/CredentialsTests.scala
+++ src/test/scala/com/busymachines/prefab/authentication/CredentialsTests.scala
@@ -2,28 +2,24 @@ package com.busymachines.prefab.authentication
 
 import org.scalatest.FlatSpec
 import com.busymachines.commons.testing.EmptyESTestIndex
-import com.busymachines.commons.event.DoNothingEventSystem
 import scala.concurrent.ExecutionContext.Implicits.global
-import com.busymachines.prefab.authentication.model.Credentials
-import com.busymachines.prefab.authentication.model.PasswordCredentials
+import com.busymachines.prefab.authentication.model.{HashFunctions, Credentials, PasswordCredentials}
 import com.busymachines.commons.logging.Logging
 import com.busymachines.prefab.authentication.elasticsearch.ESCredentialsDao
-import com.busymachines.prefab.authentication.Implicits._
 import com.busymachines.commons.Implicits.richFuture
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-@RunWith(classOf[JUnitRunner])
 class CredentialsTests extends FlatSpec with Logging {
 
   val esIndex = EmptyESTestIndex(getClass)
   val dao = new ESCredentialsDao(esIndex)
 
   "CredentialsDao" should "create & retrieve" in {
-    val credentials = Credentials(passwordCredentials = List(PasswordCredentials("login", "changeme")))
+    val credentials = Credentials(passwordCredentials = List(PasswordCredentials("login", "changeme", HashFunctions.sha256)))
     dao.create(credentials).await
     assert(dao.retrieve(credentials.id).await.get.id === credentials.id)
-    assert(dao.retrieve(credentials.id).await.get.passwordCredentials.head.hasPassword("changeme"))
+    assert(dao.retrieve(credentials.id).await.get.passwordCredentials.head.hasPassword("changeme", HashFunctions.sha256))
 
   }
 
