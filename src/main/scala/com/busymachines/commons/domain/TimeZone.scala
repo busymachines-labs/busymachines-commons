df09f6e8086413a63c3404883c4be854a180e2b1
Paul Sabou <paul.sabou@gmail.com>
2014-10-23 22:08:03 +0200

    added timezone in common domains

--- src/main/scala/com/busymachines/commons/domain/TimeZone.scala
+++ src/main/scala/com/busymachines/commons/domain/TimeZone.scala
@@ -1,12 +1,15 @@
 package com.busymachines.commons.domain
 
 import com.busymachines.commons.{EnumValue, Enum}
+import org.joda.time.DateTimeZone
 
 trait TimeZone extends EnumValue[TimeZone]
 
 object TimeZone extends Enum[TimeZone] {
 
-  case class Value(name: String, gmtOffsetInMinutes: Int) extends Val with NextId with TimeZone
+  case class Value(name: String, gmtOffsetInMinutes: Int) extends Val with NextId with TimeZone {
+    def dateTimeZone:DateTimeZone = DateTimeZone.forID(name)
+  }
 
   val Europe_Amsterdam = Value("Europe/Amsterdam", 120)
   val Europe_Berlin = Value("Europe/Berlin", 120)
