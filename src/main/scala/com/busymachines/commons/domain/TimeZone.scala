ae86c3ca32ea37f5db1d5181ed515cde7011ddb6
Paul Sabou <paul.sabou@gmail.com>
2014-10-24 09:58:50 +0200

    removed second field in time zone

--- src/main/scala/com/busymachines/commons/domain/TimeZone.scala
+++ src/main/scala/com/busymachines/commons/domain/TimeZone.scala
@@ -1,20 +1,20 @@
 package com.busymachines.commons.domain
 
-import com.busymachines.commons.{EnumValue, Enum}
+import com.busymachines.commons.{Enum, EnumValue}
 import org.joda.time.DateTimeZone
 
 trait TimeZone extends EnumValue[TimeZone]
 
 object TimeZone extends Enum[TimeZone] {
 
-  case class Value(name: String, gmtOffsetInMinutes: Int) extends Val with NextId with TimeZone {
+  case class Value(name: String) extends Val with NextId with TimeZone {
     def dateTimeZone:DateTimeZone = DateTimeZone.forID(name)
   }
 
-  val Europe_Amsterdam = Value("Europe/Amsterdam", 120)
-  val Europe_Berlin = Value("Europe/Berlin", 120)
-  val Europe_Bucharest = Value("Europe/Bucharest", 180)
-  val Europe_London = Value("Europe/London", 60)
-  val Europe_Paris = Value("Europe/Paris", 120)
+  val Europe_Amsterdam = Value("Europe/Amsterdam")
+  val Europe_Berlin = Value("Europe/Berlin")
+  val Europe_Bucharest = Value("Europe/Bucharest")
+  val Europe_London = Value("Europe/London")
+  val Europe_Paris = Value("Europe/Paris")
 
 }
