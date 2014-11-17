c4d637d1c748a1b4d3849b1e319e028d6bf72bf7
Lorand Szakacs <lorand.szakacs@busymachines.com>
2014-11-17 17:21:57 +0200

    Resource resolution is now more informative.

--- src/main/scala/com/busymachines/commons/util/ResourceResolution.scala
+++ src/main/scala/com/busymachines/commons/util/ResourceResolution.scala
@@ -1,5 +1,7 @@
 package com.busymachines.commons.util
 
+import com.busymachines.commons.CommonException
+
 /**
  * Created by Lorand Szakacs, lorand.szakacs@busymachines.com, on 11/14/14.
  *
@@ -7,6 +9,17 @@ package com.busymachines.commons.util
 trait ResourceResolution {
   protected def resourceURL(resourceName: String): java.net.URL = {
     val URL: java.net.URL = getClass.getResource(resourceName)
-    URL
+    if (URL == null) {
+      throw new CommonException(s"Failed to locate resource: '${resourceName }' from package: '${getClass.getPackage.getName }'",
+        Some("FailedToLocateResource"),
+        parameters = Map(
+          "package" -> s"${getClass.getPackage.getName }",
+          "class" -> s"${getClass.getCanonicalName }"
+        ),
+        cause = None
+      )
+    } else {
+      URL
+    }
   }
 }
