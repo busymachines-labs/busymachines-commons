5776369a7b666e116b85b641319c1c1e3351188b
Lorand Szakacs <lsz@lorandszakacs.com>
2017-08-09 10:53:06 +0300

    Tidy up constants in project/*.scala

--- project/Settings.scala
+++ project/Settings.scala
@@ -9,9 +9,12 @@ object Settings {
 
   lazy val organizationName: String = "com.busymachines"
 
+  lazy val bmCommonsHomepage: String = "https://github.com/busymachines/busymachines-commons"
+
   def commonSettings: Seq[Setting[_]] =
     Seq(
       organization in ThisBuild := organizationName,
+      homepage := Some(url(bmCommonsHomepage)),
       scalaVersion in ThisBuild := `scala_2.12`,
       crossScalaVersions in ThisBuild := seqOfCrossScalaVersions
     ) ++
