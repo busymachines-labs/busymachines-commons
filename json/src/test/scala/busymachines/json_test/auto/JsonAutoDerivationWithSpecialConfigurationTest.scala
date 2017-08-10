7eddc94ce5cd07ec90e7abed533b671904edea83
Lorand Szakacs <lsz@lorandszakacs.com>
2017-08-10 17:52:08 +0300

    Add test for configuring type discriminators in derivation

---
+++ json/src/test/scala/busymachines/json_test/auto/JsonAutoDerivationWithSpecialConfigurationTest.scala
@@ -0,0 +1,114 @@
+package busymachines.json_test.auto
+
+import busymachines.json.{Configuration, Codec, semiauto}
+import busymachines.json_test._
+import org.scalatest.FlatSpec
+
+/**
+  *
+  * @author Lorand Szakacs, lsz@lorandszakacs.com, lorand.szakacs@busymachines.com
+  * @since 10 Aug 2017
+  *
+  */
+class JsonAutoDerivationWithSpecialConfigurationTest extends FlatSpec {
+
+  import busymachines.json.syntax._
+  import busymachines.json.auto._
+
+  final implicit val _melonManiaDiscriminatorConfig: Configuration =
+    Configuration.default.withDiscriminator("_melonMania")
+
+
+  it should "... type discriminator should be _melonMania, and case object hierarchy codec should be string only" in {
+    /**
+      * We explicitly create a codec for the enumeration of case objects, thus, it should not have _melonMania discriminator
+      */
+    implicit val explicitImplicitTasteCodec: Codec[Taste] = semiauto.deriveEnumerationCodec[Taste]
+
+    val winterMelon: Melon = WinterMelon(fuzzy = true, weight = 45)
+    val waterMelon: Melon = WaterMelon(seeds = true, weight = 90)
+    val smallMelon: Melon = SmallMelon
+    val squareMelon: Melon = SquareMelon(weight = 10, tastes = Seq(SourTaste, SweetTaste))
+    val melons = List[Melon](winterMelon, waterMelon, smallMelon, squareMelon)
+
+    val rawJson = melons.asJson.spaces2
+
+    assertResult(
+      """
+        |
+        |[
+        |  {
+        |    "fuzzy" : true,
+        |    "weight" : 45,
+        |    "_melonMania" : "WinterMelon"
+        |  },
+        |  {
+        |    "seeds" : true,
+        |    "weight" : 90,
+        |    "_melonMania" : "WaterMelon"
+        |  },
+        |  {
+        |    "_melonMania" : "SmallMelon"
+        |  },
+        |  {
+        |    "weight" : 10,
+        |    "tastes" : [
+        |      "SourTaste",
+        |      "SweetTaste"
+        |    ],
+        |    "_melonMania" : "SquareMelon"
+        |  }
+        |]
+      """.stripMargin.trim
+    )(rawJson)
+
+    val read: List[Melon] = rawJson.unsafeDecodeAs[List[Melon]]
+    assertResult(melons)(read)
+  }
+
+  it should "... type discriminator should be _melonMania, and case object hierarchy serialization should also be affected by this new configuration" in {
+    val winterMelon: Melon = WinterMelon(fuzzy = true, weight = 45)
+    val waterMelon: Melon = WaterMelon(seeds = true, weight = 90)
+    val smallMelon: Melon = SmallMelon
+    val squareMelon: Melon = SquareMelon(weight = 10, tastes = Seq(SourTaste, SweetTaste))
+    val melons = List[Melon](winterMelon, waterMelon, smallMelon, squareMelon)
+
+    val rawJson = melons.asJson.spaces2
+    assertResult(
+      """
+        |
+        |[
+        |  {
+        |    "fuzzy" : true,
+        |    "weight" : 45,
+        |    "_melonMania" : "WinterMelon"
+        |  },
+        |  {
+        |    "seeds" : true,
+        |    "weight" : 90,
+        |    "_melonMania" : "WaterMelon"
+        |  },
+        |  {
+        |    "_melonMania" : "SmallMelon"
+        |  },
+        |  {
+        |    "weight" : 10,
+        |    "tastes" : [
+        |      {
+        |        "_melonMania" : "SourTaste"
+        |      },
+        |      {
+        |        "_melonMania" : "SweetTaste"
+        |      }
+        |    ],
+        |    "_melonMania" : "SquareMelon"
+        |  }
+        |]
+      """.stripMargin.trim
+    )(rawJson)
+
+    val read: List[Melon] = rawJson.unsafeDecodeAs[List[Melon]]
+    assertResult(melons)(read)
+  }
+
+}
