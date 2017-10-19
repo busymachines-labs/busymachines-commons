cc4172d4e0d20f129ea52ff9a2a991acd74348e6
Lorand Szakacs <lsz@lorandszakacs.com>
2017-10-19 19:39:26 +0300

    Add script for clearing local ivy cache

---
+++ purge-ivy.sh
@@ -0,0 +1,12 @@
+CACHE=~/.ivy2/local/com.busymachines
+LOCAL=~/.ivy2/cache/com.busymachines
+
+echo "purging local ivy cache of com.busymachines artifacts"
+echo "@ $LOCAL"
+ls -l $LOCAL
+echo "----------------"
+echo "@ $CACHE"
+ls -l $CACHE
+echo "----------------"
+rm -v -rf $LOCAL
+rm -v -rf $CACHE
