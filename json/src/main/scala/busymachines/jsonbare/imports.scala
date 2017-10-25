4d8b96285fa6e7ebb90cdcabfc65f69c1788d708
Lorand Szakacs <lsz@lorandszakacs.com>
2017-10-25 21:46:48 +0300

    Nuke jsonbare package

--- json/src/main/scala/busymachines/jsonbare/imports.scala
+++
@@ -1,9 +0,0 @@
-package busymachines.jsonbare
-
-import busymachines.json.{DefaultTypeDiscriminatorConfig, JsonSyntax, SemiAutoDerivation}
-
-object syntax extends JsonSyntax
-
-object auto extends DefaultTypeDiscriminatorConfig with io.circe.generic.extras.AutoDerivation
-
-object derive extends DefaultTypeDiscriminatorConfig with SemiAutoDerivation
