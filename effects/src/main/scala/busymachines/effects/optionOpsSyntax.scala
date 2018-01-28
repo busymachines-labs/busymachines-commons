1ee022e859b16f568fddf30f570c0df251919e2e
Lorand Szakacs <lsz@lorandszakacs.com>
2018-01-28 16:19:07 +0200

    Add Option syntax to effects #62

--- effects/src/main/scala/busymachines/effects/optionOpsSyntax.scala
+++ effects/src/main/scala/busymachines/effects/optionOpsSyntax.scala
@@ -12,11 +12,18 @@ import scala.util._
   *
   */
 trait OptionEffectsSyntaxImplicits {
+
   implicit def bmCommonsOptionEffectsSyntax[T](opt: Option[T]): OptionEffectsSyntax[T] =
     new OptionEffectsSyntax(opt)
 
   implicit def bmCommonsOptionEffectsCompanionSyntax(opt: Option.type): OptionEffectsCompanionSyntax =
     new OptionEffectsCompanionSyntax(opt)
+
+  implicit def bmCommonsBooleanAsOptionEffectsSyntax(test: Boolean): BooleanAsOptionEffectsSyntax =
+    new BooleanAsOptionEffectsSyntax(test)
+
+  implicit def bmCommonsOptionBooleanAsOptionEffectsSyntax(test: Option[Boolean]): OptionBooleanAsOptionEffectsSyntax =
+    new OptionBooleanAsOptionEffectsSyntax(test)
 }
 
 /**
@@ -43,6 +50,38 @@ final class OptionEffectsSyntax[T](val opt: Option[T]) extends AnyVal {
   def asTask(ifNone: => Anomaly): Task[T] = TaskEffectsUtil.fromOption(opt, ifNone)
 
   def asIO(ifNote: => Anomaly): IO[T] = IOEffectsUtil.fromOption(opt, ifNote)
+
+  /**
+    * Simple alias to signal how horrible it is
+    */
+  def unsafeGet: T = opt.get
+
+  def morph[R](some: T => R, ifNone: => R): Option[R] =
+    OptionEffectsUtil.morph(opt, some, ifNone)
+}
+
+/**
+  *
+  */
+final class BooleanAsOptionEffectsSyntax(val test: Boolean) extends AnyVal {
+
+  def condOption[T](whenTrue: => T): Option[T] =
+    OptionEffectsUtil.cond(test, whenTrue)
+
+  def condWithOption[T](whenTrue: => Option[T]): Option[T] =
+    OptionEffectsUtil.condWith(test, whenTrue)
+}
+
+/**
+  *
+  */
+final class OptionBooleanAsOptionEffectsSyntax(val test: Option[Boolean]) extends AnyVal {
+
+  def cond[T](whenTrue: => T): Option[T] =
+    OptionEffectsUtil.flatCond(test, whenTrue)
+
+  def condWith[T](whenTrue: => Option[T]): Option[T] =
+    OptionEffectsUtil.flatCondWith(test, whenTrue)
 }
 
 /**
@@ -69,6 +108,31 @@ final class OptionEffectsCompanionSyntax(val optObj: Option.type) extends AnyVal
   def asTask[T](opt: Option[T], ifNone: => Anomaly): Task[T] = TaskEffectsUtil.fromOption(opt, ifNone)
 
   def asIO[T](opt: Option[T], ifNote: => Anomaly): IO[T] = IOEffectsUtil.fromOption(opt, ifNote)
+
+  //===========================================================================
+  //===================== Option from special cased Option ====================
+  //===========================================================================
+
+  def cond[T](test: Boolean, whenTrue: => T): Option[T] =
+    OptionEffectsUtil.cond(test, whenTrue)
+
+  def condWith[T](test: Boolean, whenTrue: => Option[T]): Option[T] =
+    OptionEffectsUtil.condWith(test, whenTrue)
+
+  def flatCond[T](test: Option[Boolean], whenTrue: => T): Option[T] =
+    OptionEffectsUtil.flatCond(test, whenTrue)
+
+  def flatCondWith[T](test: Option[Boolean], whenTrue: => Option[T]): Option[T] =
+    OptionEffectsUtil.flatCondWith(test, whenTrue)
+
+  //===========================================================================
+  //============================== Transformers ===============================
+  //===========================================================================
+
+  // — Option.bimap does not make sense — not included
+
+  def morph[T, R](opt: Option[T], some: T => R, ifNone: => R): Option[R] =
+    OptionEffectsUtil.morph(opt, some, ifNone)
 }
 
 object OptionEffectsUtil {
@@ -82,4 +146,28 @@ object OptionEffectsUtil {
     case Some(value) => Success(value)
     case None        => Failure(throwable)
   }
+
+  def cond[T](test: Boolean, whenTrue: => T): Option[T] =
+    if (test) Option(whenTrue) else Option.empty[T]
+
+  def condWith[T](test: Boolean, whenTrue: => Option[T]): Option[T] =
+    if (test) whenTrue else Option.empty[T]
+
+  def flatCond[T](test: Option[Boolean], whenTrue: => T): Option[T] =
+    test flatMap (b => OptionEffectsUtil.cond(b, whenTrue))
+
+  def flatCondWith[T](test: Option[Boolean], whenTrue: => Option[T]): Option[T] =
+    test flatMap (b => OptionEffectsUtil.condWith(b, whenTrue))
+
+  //===========================================================================
+  //============================== Transformers ===============================
+  //===========================================================================
+
+  // — Option.bimap does not make sense — not included
+
+  def morph[T, R](opt: Option[T], some: T => R, ifNone: => R): Option[R] =
+    opt match {
+      case None        => Option(ifNone)
+      case Some(value) => Option(some(value))
+    }
 }
