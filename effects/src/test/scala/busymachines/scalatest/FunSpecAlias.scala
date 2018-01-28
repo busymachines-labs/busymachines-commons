16117727b652830ec42d731f13b7c50dee0cb773
Lorand Szakacs <lsz@lorandszakacs.com>
2018-01-28 16:19:17 +0200

    Tweak scaladoc

--- effects/src/test/scala/busymachines/scalatest/FunSpecAlias.scala
+++ effects/src/test/scala/busymachines/scalatest/FunSpecAlias.scala
@@ -4,6 +4,11 @@ import org.scalatest.FunSpec
 
 /**
   *
+  * This is the only suite that allows nesting AND can be decently run in Intellij.
+  *
+  * But being only able to use the "it" word makes for horrendous English in certain
+  * situations. Therefore this creates a simple alias for it — no extra bullshit.
+  *
   * @author Lorand Szakacs, lsz@lorandszakacs.com, lorand.szakacs@busymachines.com
   * @since 28 Jan 2018
   *
