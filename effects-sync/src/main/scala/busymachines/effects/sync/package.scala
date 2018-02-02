9d06f3e2ae33c3009b93f9b914cdd7d15ff30ad6
Lorand Szakacs <lsz@lorandszakacs.com>
2018-02-02 20:29:54 +0200

    Add copyright-header to each file

--- effects-sync/src/main/scala/busymachines/effects/sync/package.scala
+++ effects-sync/src/main/scala/busymachines/effects/sync/package.scala
@@ -1,3 +1,20 @@
+/**
+  * Copyright (c) 2017-2018 BusyMachines
+  *
+  * See company homepage at: https://www.busymachines.com/
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package busymachines.effects
 
 /**
@@ -16,10 +33,6 @@ package object sync
 
   object either extends EitherSyntax.Implicits
 
-  object result extends ResultTypeDefinitions with ResultSyntax.Implicits {
-    val Result:    busymachines.effects.sync.Result.type    = busymachines.effects.sync.Result
-    val Correct:   busymachines.effects.sync.Correct.type   = busymachines.effects.sync.Correct
-    val Incorrect: busymachines.effects.sync.Incorrect.type = busymachines.effects.sync.Incorrect
-  }
+  object result extends ResultTypeDefinitions with ResultSyntax.Implicits with ResultCompanionAliases
 
 }
