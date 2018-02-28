1f34404d295a9a2423a3be3b90f686c941bbe020
Lorand Szakacs <lsz@lorandszakacs.com>
2018-02-28 19:59:53 +0200

    Create ConstantsSyncEffects

---
+++ effects-sync/src/main/scala/busymachines/effects/sync/ConstantsSyncEffects.scala
@@ -0,0 +1,11 @@
+package busymachines.effects.sync
+
+/**
+  *
+  * @author Lorand Szakacs, lsz@lorandszakacs.com, lorand.szakacs@busymachines.com
+  * @since 28 Feb 2018
+  *
+  */
+object ConstantsSyncEffects {
+  val UnitFunction1: Any => Unit = _ => ()
+}
