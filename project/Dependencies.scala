1a96b9ce0fe16c43df68e1f20d4f19b96d0c7330
Lorand Szakacs <lsz@lorandszakacs.com>
2019-06-16 21:40:28 +0300

    Update scala to 2.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ import sbt._
 object Dependencies {
 
   lazy val `scala_2.12`:            String      = "2.12.8"
-  lazy val `scala_2.13`:            String      = "2.13.0-RC2"
+  lazy val `scala_2.13`:            String      = "2.13.0"
   lazy val mainScalaVersion:        String      = `scala_2.12`
   lazy val seqOfCrossScalaVersions: Seq[String] = Seq(`scala_2.12`, `scala_2.13`)
 
